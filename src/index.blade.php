0a9e385959a1d2e33edce03ed37ce72699a8b03a
hank <hank.huiwang@gmail.com>
2017-04-07 15:39:39 +0800

    修改目录名

--- src/index.blade.php
+++ src/index.blade.php
@@ -5,10 +5,10 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
   <title>Demo</title>
-  <link rel="stylesheet" href="/zhiyicx/plus-component-web/css/user.css" />
+  <link rel="stylesheet" href="/intervapp/plus-component-web/css/user.css" />
 </head>
 <body>
   <div id="app"></div>
-  <script src="/zhiyicx/plus-component-web/js/user.js" type="text/javascript" ></script>
+  <script src="/intervapp/plus-component-web/js/user.js" type="text/javascript" ></script>
 </body>
 </html>
