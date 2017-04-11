ce1da0f772cb62ac91564c0519d1254368316594
edwin <edwincoder@163.com>
2017-04-11 14:47:49 +0800

    app模版修改

--- src/index.blade.php
+++ src/index.blade.php
@@ -1,14 +1,32 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-  <title>Demo</title>
-  <link rel="stylesheet" href="/intervapp/plus-component-web/css/user.css" />
-</head>
-<body>
-  <div id="app"></div>
-  <script src="/intervapp/plus-component-web/js/user.js" type="text/javascript" ></script>
-</body>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>问答</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="format-detection" content="email=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="full-screen" content="yes">
+    <meta name="browsermode" content="application">
+    <meta name="x5-orientation" content="portrait">
+    <meta name="x5-fullscreen" content="true">
+    <meta name="x5-page-mode" content="app">
+    <!--清除缓存-->
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate"/>
+    <meta http-equiv="Pragma" content="no-cache"/>
+    <meta http-equiv="Expires" content="0"/>
+    <meta name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="/intervapp/plus-component-web/css/user.css" />
+  </head>
+  <body>
+    <div id="app"></div>
+    <!-- built files will be auto injected -->
+    <script src="/intervapp/plus-component-web/js/user.js" type="text/javascript" ></script>
+  </body>
 </html>
