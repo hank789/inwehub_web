42985daeadcd2a5667278289fd1d78b53218c52f
edwin <edwincoder@163.com>
2017-04-17 17:09:05 +0800

    dist index

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -43,6 +43,22 @@ var webpackConfig = merge(baseWebpackConfig, {
         safe: true
       }
     }),
+    new HtmlWebpackPlugin({
+      filename: process.env.NODE_ENV === 'testing'
+        ? 'index.html'
+        : config.build.index,
+      template: 'index.html',
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true
+        // more options:
+        // https://github.com/kangax/html-minifier#options-quick-reference
+      },
+      // necessary to consistently work with multiple chunks via CommonsChunkPlugin
+      chunksSortMode: 'dependency'
+    }),
     // new OptimizeCSSPlugin(),
     new webpack.optimize.CommonsChunkPlugin({
       name: 'manifest',
