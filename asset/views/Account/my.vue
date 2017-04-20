2a92c16203c45488f8a721e312554000cb634cf4
edwin <edwincoder@163.com>
2017-04-20 17:32:25 +0800

    个人中心

--- asset/views/Account/my.vue
+++ asset/views/Account/my.vue
@@ -2,90 +2,57 @@
 
   <div>
 
-    <div class="mui-content myinfo">
-      <ul class="mui-table-view mui-table-view-chevron">
-        <li class="mui-table-view-cell mui-media">
-          <a class="mui-navigate-right" href="#account">
-            <img class="mui-media-object mui-pull-left head-img" id="head-img" src="images/uicon.jpg">
-            <div class="mui-media-body">
-              Hello MUI
-              <p class="mui-ellipsis">账号:hellomui</p>
-            </div>
-          </a>
-        </li>
-      </ul>
+        <div class="mui-content myinfo">
+          <ul class="mui-table-view mui-table-view-chevron">
+            <li class="mui-table-view-cell mui-media">
+              <a class="mui-navigate-right" href="#account">
+                <img class="mui-media-object mui-pull-left head-img" id="head-img" src="images/uicon.jpg">
+                <div class="mui-media-body">
+                  {{ name }}
+                  <p class="mui-ellipsis">{{ phone }}</p>
+                </div>
+              </a>
+            </li>
+          </ul>
 
+          <ul class="mui-table-view mui-table-view-chevron">
+            <li class="mui-table-view-cell">
+              <a href="#privacy" class="mui-navigate-right"><span class="mui-icon mui-icon-person"></span>我的档案</a>
+            </li>
+            <li class="mui-table-view-cell">
+              <a href="#privacy" class="mui-navigate-right"><span class="mui-icon iconfont icon-vip"></span>专家管理</a>
+            </li>
+            <li class="mui-table-view-cell">
+              <a href="#general" class="mui-navigate-right"><span class="mui-icon iconfont icon-wallet"></span>我的钱包</a>
+            </li>
+          </ul>
 
-      <ul class="mui-table-view mui-table-view-chevron">
-        <li class="mui-table-view-cell">
-          <a href="#privacy" class="mui-navigate-right"><span class="mui-icon mui-icon-person"></span>我的档案</a>
-        </li>
-        <li class="mui-table-view-cell">
-          <a href="#privacy" class="mui-navigate-right"><span class="mui-icon iconfont icon-vip"></span>专家管理</a>
-        </li>
-        <li class="mui-table-view-cell">
-          <a href="#general" class="mui-navigate-right"><span class="mui-icon iconfont icon-wallet"></span>我的钱包</a>
-        </li>
-      </ul>
+          <ul class="mui-table-view mui-table-view-chevron">
+            <li class="mui-table-view-cell">
+              <a href="myAskList.html" class="mui-navigate-right"><span class="mui-icon mui-icon-help"></span>我的提问</a>
+            </li>
+            <li class="mui-table-view-cell">
+              <a href="waitAnswerList.html" class="mui-navigate-right"><span class="mui-icon iconfont icon-answer"></span>我的问答</a>
+            </li>
+          </ul>
 
-      <ul class="mui-table-view mui-table-view-chevron">
-        <li class="mui-table-view-cell">
-          <a href="myAskList.html" class="mui-navigate-right"><span class="mui-icon mui-icon-help"></span>我的提问</a>
-        </li>
-        <li class="mui-table-view-cell">
-          <a href="waitAnswerList.html" class="mui-navigate-right"><span class="mui-icon iconfont icon-answer"></span>我的问答</a>
-        </li>
-      </ul>
-
-      <ul class="mui-table-view mui-table-view-chevron mb70">
-        <li class="mui-table-view-cell">
-          <a href="#privacy" class="mui-navigate-right"><span class="mui-icon iconfont icon-share"></span>推荐给朋友</a>
-        </li>
-        <li class="mui-table-view-cell">
-          <a href="#privacy" class="mui-navigate-right"><span class="mui-icon mui-icon-gear"></span>设置与帮助</a>
-        </li>
-        <li class="mui-table-view-cell">
-          <a href="#general" class="mui-navigate-right"><span class="mui-icon-extra mui-icon-extra-lamp"></span>意见与反馈</a>
-        </li>
-        <li class="mui-table-view-cell">
-          <a href="login.html" class="mui-navigate-right"><span class="mui-icon mui-icon-close"></span>退出</a>
-        </li>
-      </ul>
-
-    </div>
+          <ul class="mui-table-view mui-table-view-chevron mb70">
+            <li class="mui-table-view-cell">
+              <a href="#privacy" class="mui-navigate-right"><span class="mui-icon iconfont icon-share"></span>推荐给朋友</a>
+            </li>
+            <li class="mui-table-view-cell">
+              <a href="#privacy" class="mui-navigate-right"><span class="mui-icon mui-icon-gear"></span>设置与帮助</a>
+            </li>
+            <li class="mui-table-view-cell">
+              <a href="#general" class="mui-navigate-right"><span class="mui-icon-extra mui-icon-extra-lamp"></span>意见与反馈</a>
+            </li>
+            <li class="mui-table-view-cell">
+              <a href="javascript:void(0)" class="mui-navigate-right" @click="logOut"><span class="mui-icon mui-icon-close"></span>退出登录</a>
+            </li>
+          </ul>
+        </div>
 
 </div>
-
-
-
-  <!--个人中心-->
-  <!--div>
-    <mu-paper class="demo-menu">
-
-      <mu-card>
-        <mu-card-header :title="name" :subTitle="phone">
-          <mu-avatar :src="avatar" slot="avatar"/>
-        </mu-card-header>
-
-        <mu-card-text>
-            总收入:0元
-        </mu-card-text>
-
-      </mu-card>
-
-      <mu-menu desktop>
-
-        <mu-divider />
-        <mu-menu-item title="联系我们" leftIcon="phone" rightIcon="keyboard_arrow_right"/>
-        <mu-menu-item title="设置" leftIcon="settings" rightIcon="keyboard_arrow_right"/>
-        <mu-menu-item title="帮助" leftIcon="help" rightIcon="keyboard_arrow_right"/>
-
-        <mu-divider />
-        <mu-menu-item @click="logOut" title="退出" leftIcon="exit_to_app" rightIcon="keyboard_arrow_right"/>
-      </mu-menu>
-    </mu-paper>
-
-  </div-->
 </template>
 <script>
   import localEvent from '../../stores/localStorage';
@@ -133,8 +100,8 @@
     margin-right: 5px;
   }
   .myinfo li{
-     padding-top: 10px;
-     padding-bottom: 10px;
+    padding-top:13px;
+    padding-bottom:13px;
   }
 
   .myinfo > .mui-table-view:first-child{
