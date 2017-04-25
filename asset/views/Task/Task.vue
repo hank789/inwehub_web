79109e6656a395b2ea839f79e4daf5423cd570ce
edwin <edwincoder@163.com>
2017-04-25 14:48:37 +0800

    新任务列表

--- asset/views/Task/Task.vue
+++ asset/views/Task/Task.vue
@@ -12,50 +12,46 @@
       </div>
     </div>
 
-    <div id="pullrefresh" class="mui-content mui-scroll-wrapper">
+    <div id="pullrefresh" class="mui-content mui-scroll-wrapper task-list">
       <div class="mui-scroll">
-
-          <div class="mui-content task-list" v-if="nothing == 0">
-
-            <div class="mui-table-view" v-for="(task, index) in tasks">
-
-              <div class="mui-table-view-cell" @tap.stop.prevent="$router.push('/answer/' + task.object_id)"
-                   v-if="task.task_type == 1">
-                <div class="title">
-                  <span class="msg">您有新的任务</span>
-                  <span class="type">{{ task.task_type_description }}</span>
-                  <span class="time"><timeago :since="timeago(task.created_at)"></timeago></span>
-                </div>
-                <div class="subTitle">
-                  <span class="questions">{{ task.description }}</span>
-                  <span class="time"></span>
-                </div>
-                <div class="link">
-                  <a>{{ task.status_description }}<span class="mui-icon mui-icon-arrowright"></span> </a>
-                </div>
+        <ul class="mui-table-view mui-table-view-chevron">
+          <template v-for="(task, index) in tasks">
+            <li class="mui-table-view-cell" @tap.stop.prevent="$router.push('/answer/' + task.object_id)"
+                 v-if="task.task_type == 1">
+              <div class="title">
+                <span class="msg">您有新的任务</span>
+                <span class="type">{{ task.task_type_description }}</span>
+                <span class="time"><timeago :since="timeago(task.created_at)"></timeago></span>
               </div>
-
-              <div class="mui-table-view-cell" @tap.stop.prevent="$router.push('/answer/' + task.object_id)"
-                   v-else-if="task.task_type == 2">
-                <div class="title">
-                  <span class="msg">您有新的任务</span>
-                  <span class="type">{{ task.task_type_description }}</span>
-                  <span class="time"><timeago :since="timeago(task.created_at)"></timeago></span>
-                </div>
-                <div class="subTitle">
-                  <span class="questions">{{ task.description }}</span>
-                  <span class="time"></span>
-                </div>
-                <div class="link">
-                  <a>{{ task.status_description }}<span class="mui-icon mui-icon-arrowright"></span> </a>
-                </div>
+              <div class="subTitle">
+                <span class="questions">{{ task.description }}</span>
+                <span class="time"></span>
               </div>
+              <div class="link">
+                <a>{{ task.status_description }}<span class="mui-icon mui-icon-arrowright"></span> </a>
+              </div>
+            </li>
 
-            </div>
-          </div>
-
+            <li class="mui-table-view-cell" @tap.stop.prevent="$router.push('/answer/' + task.object_id)"
+                 v-else-if="task.task_type == 2">
+              <div class="title">
+                <span class="msg">您有新的任务</span>
+                <span class="type">{{ task.task_type_description }}</span>
+                <span class="time"><timeago :since="timeago(task.created_at)"></timeago></span>
+              </div>
+              <div class="subTitle">
+                <span class="questions">{{ task.description }}</span>
+                <span class="time"></span>
+              </div>
+              <div class="link">
+                <a>{{ task.status_description }}<span class="mui-icon mui-icon-arrowright"></span> </a>
+              </div>
+            </li>
+          </template>
+        </ul>
       </div>
     </div>
+
 
     <div class="mui-content" v-if="nothing==1">
       <div class="mui-table-view list-ask-item">
@@ -208,21 +204,22 @@
 
 
 <style scoped>
-  .task-list {
+  .task-list{
     line-height: 33px;
+    margin-top:15px;
   }
-
-  .task-list .time {
+  .task-list .mui-table-view-chevron .mui-table-view-cell{
+    padding-right:5px;
+  }
+  .task-list .time{
     float: right;
-    color: #999;
+    color:#999;
   }
-
-  .task-list .link a {
+  .task-list .link a{
     color: #8f8f94;
     font-size: 14px;
   }
-
-  .task-list .type {
-    font-weight: bold;
+  .task-list .type{
+    font-weight:bold;
   }
 </style>
