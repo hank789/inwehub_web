0c2c53da2c9ebb14a53388daa507bb4ac11f2594
edwin <edwincoder@163.com>
2017-05-03 19:43:46 +0800

    截取头像修改

--- asset/views/Account/selectHeader.vue
+++ asset/views/Account/selectHeader.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  <img :src="localUrl" width="200" height="200" id="image"/><br/>
+  <img :src="localUrl" width="100%" height="200" id="image"/><br/>
   <button type="button" class="mui-btn  mui-btn-block"
           @tap.stop.prevent="cancel">取消</button>
   <button type="button" class="mui-btn  mui-btn-block"
@@ -15,17 +15,27 @@
   export default {
     data:() => ({
         localUrl:'/images/uicon.jpg',
-        cropper:null
+        cropper:null,
+        img:{
+          width:0,
+          height:0,
+          x:0,
+          y:0
+        }
     }),
     created(){
-       //this.localUrl =  '/images/uicon.jpg';this.$route.query.src;
+       this.localUrl = this.$route.query.src;
     },
     mounted(){
-      this.clip();
+      var t = this;
+      mui.plusReady(function () {
+        t.clip();
+      });
     },
     methods: {
       clip(){
         var image = document.getElementById('image');
+        var t = this;
         this.cropper = new Cropper(image, {
           aspectRatio: 1/1,
           dragMode:'move',
@@ -37,6 +47,10 @@
           minContainerWidth:200,
           minContainerHeight:200,
           crop: function(data) {
+            t.img.x = data.detail.x;
+            t.img.y = data.detail.y;
+            t.img.width = data.detail.width;
+            t.img.height = data.detail.height;
           }
         });
       },
@@ -44,11 +58,23 @@
         this.$router.go(-1);
       },
       submit(){
-        var resImg =  this.cropper.getCroppedCanvas({
-          width: 200,
-          height: 200
-        }).toDataURL();
-        mui.alert(resImg);
+        var t = this;
+        plus.zip.compressImage(
+          {
+            src: this.localUrl, //src在这里是第一步Url里的src。也就是本地路径
+            dst: '_doc/a.jpg',
+            overwrite: true,
+            clip: {
+              top: this.img.y + 'px',
+              left: this.img.x + 'px',
+              width: this.img.width + 'px',
+              height: this.img.height + 'px'
+            }
+          },
+          function(e) {
+            mui.alert(e.target); //压缩图片
+          }
+        );
       }
     }
   }
