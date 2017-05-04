81971e3a6c5a44c666d07290114a40eb82d1cbf2
edwin <edwincoder@163.com>
2017-05-04 14:49:57 +0800

    消息列表页加返回

--- asset/views/message.vue
+++ asset/views/message.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <header class="mui-bar mui-bar-nav">
-      <a class="mui-action-back mui-icon mui-icon-left-nav mui-pull-left"></a>
+      <a class="mui-action-back mui-icon mui-icon-left-nav mui-pull-left" @tap.stop.prevent="$router.go(-1)"></a>
       <h1 class="mui-title">消息</h1>
     </header>
 
