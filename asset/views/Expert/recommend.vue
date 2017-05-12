11922b02d3d7a74cf22ce1debb4228ba7c5c1900
edwin <edwincoder@163.com>
2017-05-12 10:40:43 +0800

    添加首页v2

--- asset/views/Expert/recommend.vue
+++ asset/views/Expert/recommend.vue
@@ -55,12 +55,8 @@
         上传专家名片：
       </div>
       <div id='image-list' class="row image-list"></div>
-      <div class="mui-table-view">
-        <div class="mui-table-view-cell footer">
-          <button type="button" id="submit" class="mui-btn-block mui-btn-primary">
-            提交
-          </button>
-        </div>
+      <div class="button-wrapper">
+        <button type="button" class="mui-btn mui-btn-block mui-btn-primary">提交</button>
       </div>
     </div>
 
@@ -97,176 +93,6 @@
 
     },
     mounted(){
-      mui('.mui-switch')['switch']();
-      var imageIndexIdNum = 0;
-      var index = 1;
-      var feedback = {
-        imageList: document.getElementById('image-list'),
-        submitBtn: document.getElementById('submit'),
-        description: document.getElementById('description'),
-        name: document.getElementById('name'),
-        gender: document.getElementById('gender'),
-        work_years: document.getElementById('work_years'),
-        mobile: document.getElementById('mobile'),
-        industry_tags: document.getElementById('industry_tags')
-      };
-      feedback.files = [];
-      feedback.uploader = null;
-      feedback.getFileInputArray = function() {
-        return [].slice.call(feedback.imageList.querySelectorAll('.file'));
-      };
-      feedback.addFile = function(path) {
-        feedback.files.push({name:"images"+index,path:path,id:"img-"+index});
-        index++;
-      };
-      /**
-       * 初始化图片域占位
-       */
-      feedback.newPlaceholder = function() {
-        var fileInputArray = feedback.getFileInputArray();
-        if (fileInputArray &&
-          fileInputArray.length > 0 &&
-          fileInputArray[fileInputArray.length - 1].parentNode.classList.contains('space')) {
-          return;
-        };
-        imageIndexIdNum++;
-        var placeholder = document.createElement('div');
-        placeholder.setAttribute('class', 'image-item space');
-        var up = document.createElement("div");
-        up.setAttribute('class','image-up')
-        //删除图片
-        var closeButton = document.createElement('div');
-        closeButton.setAttribute('class', 'image-close');
-        closeButton.innerHTML = 'X';
-        closeButton.id = "img-"+index;
-        //小X的点击事件
-        closeButton.addEventListener('tap', function(event) {
-          setTimeout(function() {
-            for(var temp=0;temp<feedback.files.length;temp++){
-              if(feedback.files[temp].id==closeButton.id){
-                feedback.files.splice(temp,1);
-              }
-            }
-            feedback.imageList.removeChild(placeholder);
-          }, 0);
-          return false;
-        }, false);
-
-        //
-        var fileInput = document.createElement('div');
-        fileInput.setAttribute('class', 'file');
-        fileInput.setAttribute('id', 'image-' + imageIndexIdNum);
-        fileInput.addEventListener('tap', function(event) {
-          var self = this;
-          var index = (this.id).substr(-1);
-
-          plus.gallery.pick(function(e) {
-//				console.log("event:"+e);
-            var name = e.substr(e.lastIndexOf('/') + 1);
-            console.log("name:"+name);
-
-            plus.zip.compressImage({
-              src: e,
-              dst: '_doc/' + name,
-              overwrite: true,
-              quality: 50
-            }, function(zip) {
-              size += zip.size
-              console.log("filesize:"+zip.size+",totalsize:"+size);
-              if (size > (10*1024*1024)) {
-                return mui.toast('文件超大,请重新选择~');
-              }
-              if (!self.parentNode.classList.contains('space')) { //已有图片
-                feedback.files.splice(index-1,1,{name:"images"+index,path:e});
-              } else { //加号
-                placeholder.classList.remove('space');
-                feedback.addFile(zip.target);
-                feedback.newPlaceholder();
-              }
-              up.classList.remove('image-up');
-              placeholder.style.backgroundImage = 'url(' + zip.target + ')';
-            }, function(zipe) {
-              mui.toast('压缩失败！')
-            });
-
-
-
-          }, function(e) {
-            mui.toast(e.message);
-          },{});
-        }, false);
-        placeholder.appendChild(closeButton);
-        placeholder.appendChild(up);
-        placeholder.appendChild(fileInput);
-        feedback.imageList.appendChild(placeholder);
-      };
-
-      feedback.newPlaceholder();
-
-      feedback.submitBtn.addEventListener('tap', function(event) {
-
-        if (feedback.description.value.length > 2000) {
-          return mui.toast('信息超长,请重新填写~')
-        }
-        //判断网络连接
-        if(plus.networkinfo.getCurrentType()==plus.networkinfo.CONNECTION_NONE){
-          return mui.toast("连接网络失败，请稍后再试");
-        }
-        feedback.send(mui.extend({}, feedback.deviceInfo, {
-          description: feedback.description.value,
-          name: feedback.name.value,
-          gender: feedback.gender.value,
-          work_years: feedback.work_years.value,
-          mobile: feedback.mobile.value,
-          industry_tags: feedback.industry_tags.value,
-          images: feedback.files,
-        }))
-      }, false);
-
-      feedback.send = function(content) {
-        feedback.uploader = plus.uploader.createUpload(createAPI('expert/apply'), {
-          method: 'POST'
-        }, function(upload, status) {
-//			plus.nativeUI.closeWaiting()
-          console.log("upload cb:"+upload.responseText);
-          if(status==200){
-            var data = JSON.parse(upload.responseText);
-            //上传成功，重置表单
-            if (data.status === 1000) {
-//					mui.toast('反馈成功~')
-              console.log("upload success");
-//					feedback.clearForm();
-            }
-          }else{
-            console.log("upload fail");
-          }
-
-        });
-        const UserLoginInfo = localEvent.getLocalItem('UserLoginInfo');
-        feedback.uploader.setRequestHeader('Authorization', 'bearer ' + UserLoginInfo.token);
-        //添加上传数据
-        mui.each(content, function(index, element) {
-          if (index !== 'images') {
-            console.log("addData:"+index+","+element);
-//				console.log(index);
-            feedback.uploader.addData(index, element)
-          }
-        });
-        //添加上传文件
-        mui.each(feedback.files, function(index, element) {
-          var f = feedback.files[index];
-          console.log("addFile:"+JSON.stringify(f));
-          feedback.uploader.addFile(f.path, {
-            key: f.name
-          });
-        });
-        //开始上传任务
-        feedback.uploader.start();
-        mui.alert("感谢推荐，点击确定关闭","专家推荐","确定",function () {
-          mui.back();
-        });
-//		plus.nativeUI.showWaiting();
-      };
 
     }
   }
@@ -279,21 +105,8 @@
     margin-top: 15px;
   }
 
-  .myinfo li{
-    color:#101010;
-    padding-top:13px;
-    padding-bottom:13px;
-  }
-
-  .myinfo > .mui-table-view:first-child{
-    margin-top:0;
-  }
-
-  .my-header p{
-    margin-top:5px;
-    color:#fff;
-    font-size:16px;
-    font-weight:bold;
+  .mui-table-view-cell{
+    padding:0;
   }
 
   .expert-setting-field {
@@ -398,5 +211,8 @@
     font-family: "微软雅黑";
     content: '+';
     font-size: 60px;
+  }
+  .button-wrapper{
+    padding:0 50px;
   }
 </style>
--- asset/views/Home.vue
+++ asset/views/Home.vue
@@ -1,17 +1,328 @@
 <template>
   <div>
+
+    <header class="mui-bar mui-bar-nav">
+      <h1 class="mui-title">InweHub</h1>
+    </header>
+
+
     <div class="mui-content">
-      <div class="button-wrapper">
-        <button type="button" class="mui-btn mui-btn-block mui-btn-primary" @tap.stop.prevent="$router.push('/ask')">提问</button>
+      <div class="homeWrapper">
+        <div class="topWrapper">
+          <div class="left">
+            <div class="title">资深专家推荐</div>
+          </div>
+          <div class="right">
+            <div class="item apply">
+              <div class="buttons">
+                <span class="mui-icon fa fa-check"></span><span class="mui-icon fa fa-chevron-right"></span>
+              </div>
+              申请专家
+
+
+            </div>
+
+            <div class="item recommend">
+              <div class="buttons">
+                <span class="mui-icon fa fa-check"></span><span class="mui-icon fa fa-chevron-right"></span>
+              </div>
+              推荐专家
+
+
+            </div>
+          </div>
+        </div>
+        <div class="ask"><span class="title">本周优质提问</span><span class="mui-icon fa fa-refresh"></span></div>
+        <div class="askList">
+          <div class="person">
+            <div class="avatar">
+              <div class="avatarInner">
+                <img src="images/uicon.jpg"/>
+              </div>
+            </div>
+            <div class="mui-media-body">
+              <span class="username">我是二x哈</span>
+              <span class="status">专家已回答</span>
+              <div class="mui-ellipsis-2">
+                SAP HANA S/4中有关财务方面的事务代表，相比较与R3，取消了哪些之前常用的？
+
+
+              </div>
+              <span class="amount">奖励180元</span>
+            </div>
+          </div>
+          <div class="person">
+            <div class="avatar">
+              <div class="avatarInner">
+                <img src="images/uicon.jpg"/>
+              </div>
+            </div>
+            <div class="mui-media-body">
+              <span class="username">我是二x哈</span>
+              <span class="status">专家已回答</span>
+              <div class="mui-ellipsis-2">
+                SAP HANA S/4中有关财务方面的事务代表，相比较与R3，取消了哪些之前常用的？
+                <span class="amount">奖励180元</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="answer"><span class="title">SAP行业专业问答</span><span class="label" style="display: none;">优质提问返现</span>
+        </div>
+
+
+        <div class="tags"><span>专业</span><span>|</span><span>高效</span><span>|</span><span>准确</span></div>
+        <div class="askButton">
+          <button type="button" class="mui-btn mui-btn-block mui-btn-primary" @tap.stop.prevent="$router.push('/ask')">
+            立即提问<br/><span>平台服务保障</span></button>
+        </div>
+        <div class="tabs">
+          <div class="tabs-item">
+            <span class="mui-icon fa fa-user-secret"></span>
+            <span class="text">147名行业专家</span>
+          </div>
+          <div class="tabs-item">
+            <span class="mui-icon fa fa-clock-o"></span>
+            <span class="text">平均2小时应答</span>
+          </div>
+          <div class="tabs-item">
+            <span class="mui-icon fa fa-thumbs-up"></span>
+            <span class="text">跨越67个行业</span>
+          </div>
+        </div>
+        <div class="mb70"></div>
       </div>
     </div>
+
   </div>
 </template>
 
 
 <style scoped>
-  .button-wrapper{
-    margin:10px;
+  .homeWrapper{
+    border-bottom:1px solid #f2f2f2;
+  }
+  .topWrapper {
+    height: 164px;
+    display: -webkit-flex;
+    display: flex;
+    background: #fff;
   }
 
+  .topWrapper .left {
+    position: relative;
+    flex-grow: 1;
+    -webkit-flex-grow: 1;
+  }
+
+  .topWrapper .left .title {
+    font-weight: bold;
+    position: absolute;
+    left: 10px;
+    top: 10px;
+  }
+
+  .topWrapper .right {
+    width: 101px;
+    border: 1px solid #f2f2f2;
+  }
+
+  .topWrapper .right .item {
+    height: 82px;
+    border: 1px solid #f2f2f2;
+    text-align: center;
+    padding-top: 10px;
+  }
+
+  .topWrapper .buttons {
+    position: relative;
+    margin-bottom: 5px;
+  }
+
+  .topWrapper .fa-check {
+    color: #587dd5;
+    font-size: 32px;
+    opacity: 0.5;
+  }
+
+  .topWrapper .fa-chevron-right {
+    color: #bbb;
+    position: relative;
+    top: -7px;
+    right: -7px;
+    font-size: 18px;
+  }
+
+  .ask {
+    height: 35px;
+    line-height: 35px;
+    position: relative;
+  }
+
+  .ask .title {
+    padding-left: 10px;
+    font-weight: bold;
+  }
+
+  .ask .mui-icon {
+    font-size: 24px;
+    color: #587dd5;
+    position: absolute;
+    right: 20px;
+    top: 5px;
+  }
+
+  .askList {
+    background: #fff;
+  }
+
+  .askList .avatar {
+    z-index: 0;
+    color: #ffffff;
+    float: left;
+    display: inline-block;
+    margin-top: 3px;
+    margin-right: 10px;
+    height: 32px;
+    width: 32px;
+    font-size: 20px;
+    text-align: center;
+    border-radius: 50%;
+  }
+
+  .askList .avatarInner {
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    width: 100%;
+    height: 100%;
+    -webkit-box-align: center;
+    -webkit-align-items: center;
+    -ms-flex-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    -webkit-justify-content: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+  }
+
+  .askList .avatar img {
+    border-radius: 50%;
+    width: 100%;
+    height: 100%;
+    display: block;
+  }
+
+  .askList .username, .askList .status {
+    color: #a8a8a8;
+    font-size: 12px;
+  }
+
+  .askList .mui-media-body {
+    position: relative;
+    padding-left: 10px;
+  }
+
+  .askList .mui-media-body div {
+    color: #555;
+    font-size: 12px;
+  }
+
+  .askList .mui-ellipsis-2 {
+    margin-top: 5px;
+  }
+
+  .askList .amount {
+    background: #e26153;
+    color: #fff;
+    border-radius: 5px;
+    font-size: 12px;
+    padding: 1px 5px;
+    position: absolute;
+    right: 0;
+    top: 3px;
+  }
+
+  .askList .person {
+    margin: 0px 10px;
+    padding: 10px 0;
+    border-top: 1px solid #f2f2f2;
+  }
+
+  .answer {
+    position: relative;
+    height: 35px;
+    line-height: 35px;
+
+  }
+
+  .answer .title {
+    font-weight: bold;
+    margin-left: 10px;
+  }
+
+  .answer .label {
+    background: #e26153;
+    color: #fff;
+    border-radius: 5px;
+    font-size: 12px;
+    padding: 1px 5px;
+    position: absolute;
+    right: 0;
+    top: 3px;
+  }
+
+  .tabs {
+    margin-top: 20px;
+    padding-bottom: 10px;
+  }
+
+  .tabs .tabs-item {
+    display: inline-block;
+    text-align: center;
+    width: 32%;
+  }
+
+  .tabs .tabs-item .mui-icon {
+    display: block;
+    color: #587dd5;
+    font-size: 32px;
+    margin-bottom: 10px;
+  }
+
+  .tabs .tabs-item .text {
+    font-size: 14px;
+
+  }
+
+  .tags {
+    padding-left: 15px;
+  }
+
+  .tags span {
+    font-size: 14px;
+    margin: 0 5px;
+  }
+
+  .askButton {
+    margin-top: 10px;
+    padding: 0 50px;
+  }
+
+  .askButton button {
+    padding: 5px 0;
+    border-radius: 8px;
+    line-height: 22px;
+    font-size: 22px;
+  }
+
+  .askButton button span {
+    font-size: 14px;
+  }
+
+  .mui-content {
+    background: #fff;
+  }
 </style>
