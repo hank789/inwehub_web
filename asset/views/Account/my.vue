ba86d95d9de7750043acc3acac3a5616ff536df8
edwin <edwincoder@163.com>
2017-05-25 18:40:21 +0800

    添加我的页面

--- asset/views/Account/my.vue
+++ asset/views/Account/my.vue
@@ -17,7 +17,7 @@
           </div>
         </div>
         <div class="text">
-          <div class="realname">郭大红</div>
+          <div class="realname">{{ name }}</div>
           <div class="label" v-show="isExpert"><span class="mui-icon myicon myicon-gaojizhuanjia"></span>高级专家</div>
           <div class="options">
             <div class="buttonAsk" @tap.stop.prevent="$router.push('/my/info')"><span>{{ account_info_complete_percent }}%</span> 编辑名片</div>
