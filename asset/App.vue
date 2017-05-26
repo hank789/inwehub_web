c7b96e47c006aba544ab33fa2ceba0fb20728aec
edwin <edwincoder@163.com>
2017-05-26 11:37:55 +0800

    首页添加专家导航和左边栏

--- asset/App.vue
+++ asset/App.vue
@@ -1,39 +1,90 @@
 <template>
   <div id="app">
-    <div v-wechat-title="$route.meta.title"></div>
 
-    <div class='view'>
-      <transition name='none' mode="out-in">
-        <router-view></router-view>
-      </transition>
+    <div class="mui-off-canvas-wrap mui-draggable mui-slide-in">
+
+      <!-- 菜单容器 -->
+      <aside class="mui-off-canvas-left mySideMenu">
+        <div class="mui-scroll-wrapper">
+          <div class="mui-scroll">
+            <!-- 菜单具体展示内容 -->
+            ...
+
+
+
+          </div>
+        </div>
+
+      </aside>
+
+
+
+      <!-- 主页面容器 -->
+      <div class="mui-inner-wrap">
+
+
+        <div v-wechat-title="$route.meta.title"></div>
+
+        <div class='view'>
+          <transition name='none' mode="out-in">
+            <router-view></router-view>
+          </transition>
+        </div>
+
+        <nav class="mui-bar mui-bar-tab footer-bar" v-show='showBottom'>
+          <div class="mui-tab-item" @tap.stop.prevent="linkTo('/')" :class="{ 'mui-active' : isHome}">
+            <span class="mui-icon myicon myicon-home"></span>
+            <div><span class="mui-icon myicon myicon-point" v-show="isHome"></span></div>
+
+          </div>
+
+          <div class="mui-tab-item" @tap.stop.prevent="linkTo('/task')" :class="{ 'mui-active' : isAsk}">
+            <span class="mui-icon myicon myicon-task"><span class="mui-badge" v-if="taskCount">{{ taskCount
+              }}</span></span>
+            <div><span class="mui-icon myicon myicon-point" v-show="isAsk"></span></div>
+
+          </div>
+
+          <div class="mui-tab-item" @tap.stop.prevent="linkTo('/discover')" :class="{ 'mui-active' : isDiscover}">
+            <span class="mui-icon myicon myicon-find"></span>
+            <div><span class="mui-icon myicon myicon-point" v-show="isDiscover"></span></div>
+
+          </div>
+
+          <div class="mui-tab-item" @tap.stop.prevent="linkTo('/my')" :class="{ 'mui-active':isMy}">
+            <span class="mui-icon myicon myicon-my"></span>
+            <div><span class="mui-icon myicon myicon-point" v-show="isMy"></span></div>
+
+          </div>
+        </nav>
+
+        <div id="backdrop" class="mui-off-canvas-backdrop"></div>
+
+
+      </div>
     </div>
 
-    <nav class="mui-bar mui-bar-tab footer-bar" v-show='showBottom'>
-      <div class="mui-tab-item" @tap.stop.prevent="linkTo('/')" :class="{ 'mui-active' : isHome}">
-        <span class="mui-icon myicon myicon-home"></span><div><span class="mui-icon myicon myicon-point" v-show="isHome"></span></div>
+    <div id="expert" class="mui-popover mui-popover-action mui-popover-bottom">
+      <ul class="mui-table-view">
+        <li class="mui-table-view-cell">
+          <a @tap.stop.prevent="goRecommand">推荐专家</a>
+        </li>
+        <li class="mui-table-view-cell">
+          <a @tap.stop.prevent="goExpert">专家管理</a>
+        </li>
+      </ul>
+      <ul class="mui-table-view">
+        <li class="mui-table-view-cell">
+          <a @tap.stop.prevent="expertNav"><b>取消</b></a>
+        </li>
+      </ul>
+    </div>
 
-      </div>
-
-      <div class="mui-tab-item" @tap.stop.prevent="linkTo('/task')" :class="{ 'mui-active' : isAsk}">
-        <span class="mui-icon myicon myicon-task"><span class="mui-badge" v-if="taskCount">{{ taskCount }}</span></span><div><span class="mui-icon myicon myicon-point" v-show="isAsk"></span></div>
-
-      </div>
-
-      <div class="mui-tab-item" @tap.stop.prevent="linkTo('/discover')" :class="{ 'mui-active' : isDiscover}">
-        <span class="mui-icon myicon myicon-find"></span><div><span class="mui-icon myicon myicon-point" v-show="isDiscover"></span></div>
-
-      </div>
-
-      <div class="mui-tab-item" @tap.stop.prevent="linkTo('/my')" :class="{ 'mui-active':isMy}">
-        <span class="mui-icon myicon myicon-my"></span><div><span class="mui-icon myicon myicon-point" v-show="isMy"></span></div>
-
-      </div>
-    </nav>
   </div>
 </template>
 
 <script>
-  import { mapGetters } from 'vuex'
+  import {mapGetters} from 'vuex'
   import {createAPI, addAccessToken, postRequest} from './utils/request';
   import localEvent from './stores/localStorage';
 
@@ -42,19 +93,30 @@
       return {
         isHome: false,
         isAsk: false,
-        isMy:false,
-        showBottom:true,
-        div:false,
-        isDiscover:false,
-        taskCount:0
+        isMy: false,
+        showBottom: true,
+        div: false,
+        isDiscover: false,
+        taskCount: 0
       }
     },
     methods: {
+      goRecommand: function () {
+        this.expertNav();
+        this.$router.push('/expert/recommend')
+      },
+      goExpert: function () {
+        this.expertNav();
+        this.$router.push('/expert')
+      },
+      expertNav: function () {
+        mui('#expert').popover('toggle');
+      },
       getCount(){
 
         let UserLoginInfo = localEvent.getLocalItem('UserLoginInfo');
         if (!UserLoginInfo.token) {
-            return;
+          return;
         }
 
         postRequest(`notification/count`, {}).then(response => {
@@ -67,18 +129,18 @@
         });
       },
       linkTo(dest){
-         this.$router.push(dest);
+        this.$router.push(dest);
       },
       changeNav(path, fullPath)
       {
-        var curPath = path == ''?'home':path;
+        var curPath = path == '' ? 'home' : path;
         this.isHome = this.isAsk = this.isMy = this.isDiscover = false;
         this.showBottom = true;
-        mui.each(mui(".mui-tab-item"), function(index,item){
+        mui.each(mui(".mui-tab-item"), function (index, item) {
           item.className = "mui-tab-item";
         });
 
-        switch(fullPath) {
+        switch (fullPath) {
           case '/home':
             this.isHome = true;
             break;
@@ -102,13 +164,13 @@
     },
     created(){
       var tmpArr = this.$route.path.split('/')
-      var curPath = tmpArr[1] == ''?'home':tmpArr[1];
+      var curPath = tmpArr[1] == '' ? 'home' : tmpArr[1];
       this.changeNav(curPath, this.$route.path);
     },
     watch: {
       $route(to) {
         var tmpArr = to.path.split('/');
-        var curPath = tmpArr[1] == ''?'home':tmpArr[1];
+        var curPath = tmpArr[1] == '' ? 'home' : tmpArr[1];
         this.changeNav(curPath, this.$route.path);
       }
     },
@@ -116,60 +178,60 @@
       var router = this.$router;
 
       //监听推送
-      mui.plusReady(function() {
+      mui.plusReady(function () {
         if (mui.os.plus) {
 
           plus.navigator.setStatusBarBackground("#161616");
           plus.navigator.setStatusBarStyle('light');
 
-          var noticeTo = function(payload){
-            switch(payload.object_type) {
-                 case 'question':
-                     router.push('/ask/'+payload.object_id);
-                     break;
-                 case 'answer':
-                     router.push('/answer/'+payload.object_id);
-                     break;
-               }
+          var noticeTo = function (payload) {
+            switch (payload.object_type) {
+              case 'question':
+                router.push('/ask/' + payload.object_id);
+                break;
+              case 'answer':
+                router.push('/answer/' + payload.object_id);
+                break;
+            }
           };
 
           // 监听点击消息事件
-          plus.push.addEventListener( "click", ( msg ) => {
+          plus.push.addEventListener("click", (msg) => {
             // 判断是从本地创建还是离线推送的消息
-            switch( msg.payload ) {
+            switch (msg.payload) {
               case "LocalMSG":
-                console.log( "点击本地创建消息启动：" );
+                console.log("点击本地创建消息启动：");
                 break;
               default:
                 console.log(msg);
                 break;
             }
             // 提示点击的内容
-            if ( msg.payload ) {
+            if (msg.payload) {
               //plus.nativeUI.alert( "click:payload(JSON): "+JSON.stringify(msg.payload) );
               if (mui.os.ios) {
-                var payload =  msg.payload;
+                var payload = msg.payload;
               } else {
-                var payload =  JSON.parse(msg.payload);
+                var payload = JSON.parse(msg.payload);
               }
               noticeTo(payload);
             }
 
-          }, false );
+          }, false);
           // 监听在线消息事件
-          plus.push.addEventListener( "receive", ( msg ) => {
-            if ( msg.aps ) {  // Apple APNS message
-              console.log( "接收到在线APNS消息：" );
+          plus.push.addEventListener("receive", (msg) => {
+            if (msg.aps) {  // Apple APNS message
+              console.log("接收到在线APNS消息：");
             } else {
-              console.log( "接收到在线透传消息：" );
+              console.log("接收到在线透传消息：");
             }
 
-            if ( msg.payload ) {
+            if (msg.payload) {
               //plus.nativeUI.alert( "receive:payload: "+JSON.stringify(msg.payload) );
               if (mui.os.ios) {
-                var payload =  msg.payload.payload;
+                var payload = msg.payload.payload;
               } else {
-                var payload =  JSON.parse(msg.payload);
+                var payload = JSON.parse(msg.payload);
               }
 
               var btnArray = ['取消', '前往查看'];
@@ -180,7 +242,7 @@
               });
             }
 
-          }, false );
+          }, false);
         }
       });
     }
@@ -188,24 +250,26 @@
 </script>
 
 <style scoped>
-  .footer-bar{
-    background-color:#fff;
+  .footer-bar {
+    background-color: #fff;
     box-shadow: none;
   }
-  .footer-bar{
+
+  .footer-bar {
     border-top: 1px solid #D9D9D9;
   }
 
-  .footer-bar .mui-tab-item{
-    position:relative;
+  .footer-bar .mui-tab-item {
+    position: relative;
   }
 
-  .footer-bar .mui-active div{
+  .footer-bar .mui-active div {
     position: absolute;
-    width:100%;
-    bottom:5px;
+    width: 100%;
+    bottom: 5px;
   }
-  .footer-bar .mui-active .myicon-point{
+
+  .footer-bar .mui-active .myicon-point {
     width: 3px;
     height: 3px;
   }
--- asset/views/Home.vue
+++ asset/views/Home.vue
@@ -1,78 +1,99 @@
 <template>
   <div>
-  <header class="mui-bar mui-bar-dark mui-bar-nav">
-    <h1 class="mui-title">InweHub</h1>
-  </header>
-
-  <div class="mui-content">
-    <div class="professor">
-      <div class="avatar">
-        <div class="avatarInner">
-          <img src="../statics/images/uicon.jpg" class="avatar"/>
-        </div>
-      </div>
-      <div class="text">
-        <div class="realname">郭大红</div>
-        <div class="label"><div class="collect"><span class="mui-icon fa fa-vimeo"></span></div>今日专家</div>
-        <div class="options">
-          <div class="buttonAsk" @tap.stop.prevent="$router.push('/ask')"><span>+</span> 向他提问</div>
-          <div class="collect"><span class="mui-icon fa fa-heart-o"></span></div>
-          <div class="collect"><span class="mui-icon fa fa-share"></span></div>
-        </div>
-      </div>
-    </div>
-
-    <div class="professorDesc">
-      <div class="title"><span class="icon"></span>专家介绍</div>
-      <div class="detail mui-ellipsis-3">SAP咨询行业15年从业经历，熟悉离散制造行业，专注pp等模块，是一位非常自身的超级顾问。写三行，永远写三行都写三行。都写三行。都写三行。都写三行。都写三行。</div>
-    </div>
 
 
-    <div class="bountyDesc">
-      <div class="title">
-        <span class="icon"></span>奖励问答
-            <span class="titleSub">优质问与答将会获现金奖励</span>
-        <span class="more mui-navigate-right">更多</span>
-      </div>
-    </div>
+    <header class="mui-bar mui-bar-dark mui-bar-nav">
+      <a class="mui-icon myicon myicon-qian" @tap.stop.prevent="toggleMenu"></a>
+      <h1 class="mui-title">InweHub</h1>
+      <a class="mui-icon myicon myicon-expert mui-pull-right" @tap.stop.prevent="expertNav"></a>
+    </header>
+
+    <div class="mui-content">
 
 
-    <div class="bountyDescList">
-
-      <div class="detail">
-        <div class="image">
-          <img src="../statics/images/home-youzhitiwen.png"/>
+      <div class="professor">
+        <div class="avatar">
+          <div class="avatarInner">
+            <img src="../statics/images/uicon.jpg" class="avatar"/>
+          </div>
         </div>
         <div class="text">
-          <div class="content mui-ellipsis-3">
-            SAP咨询行业15年从业经历，熟悉离散制造行业，好回答的问题标题，哈哈哈哈合肥市说u 发挥绥芬河市东方。
-        </div>
-          <div class="realname">Mike zhang</div>
-          <div class="amount">额外奖励100元</div>
+          <div class="realname">郭大红</div>
+          <div class="label">
+            <div class="collect"><span class="mui-icon fa fa-vimeo"></span></div>
+            今日专家
+
+          </div>
+          <div class="options">
+            <div class="buttonAsk" @tap.stop.prevent="$router.push('/ask')"><span>+</span> 向他提问</div>
+            <div class="collect"><span class="mui-icon fa fa-heart-o"></span></div>
+            <div class="collect"><span class="mui-icon fa fa-share"></span></div>
+          </div>
         </div>
       </div>
 
-      <div class="detail">
-        <div class="image">
-          <img src="../statics/images/home_youzhihuida.png"/>
-        </div>
-        <div class="text">
-          <div class="content mui-ellipsis-3">
-            SAP咨询行业15年从业经历，熟悉离散制造行业，好回答的问题标题，哈哈哈哈合肥市说u 发挥绥芬河市东方。
-        </div>
-          <div class="realname">Mike zhang</div>
-          <div class="amount">额外奖励100元</div>
+      <div class="professorDesc">
+        <div class="title"><span class="icon"></span>专家介绍</div>
+        <div class="detail mui-ellipsis-3">
+          SAP咨询行业15年从业经历，熟悉离散制造行业，专注pp等模块，是一位非常自身的超级顾问。写三行，永远写三行都写三行。都写三行。都写三行。都写三行。都写三行。
+
         </div>
       </div>
+
+
+      <div class="bountyDesc">
+        <div class="title">
+          <span class="icon"></span>奖励问答
+                <span class="titleSub">优质问与答将会获现金奖励</span>
+          <span class="more mui-navigate-right">更多</span>
+        </div>
+      </div>
+
+
+      <div class="bountyDescList">
+
+        <div class="detail">
+          <div class="image">
+            <img src="../statics/images/home-youzhitiwen.png"/>
+          </div>
+          <div class="text">
+            <div class="content mui-ellipsis-3">
+              SAP咨询行业15年从业经历，熟悉离散制造行业，好回答的问题标题，哈哈哈哈合肥市说u 发挥绥芬河市东方。
+
+
+            </div>
+            <div class="realname">Mike zhang</div>
+            <div class="amount">额外奖励100元</div>
+          </div>
+        </div>
+
+        <div class="detail">
+          <div class="image">
+            <img src="../statics/images/home_youzhihuida.png"/>
+          </div>
+          <div class="text">
+            <div class="content mui-ellipsis-3">
+              SAP咨询行业15年从业经历，熟悉离散制造行业，好回答的问题标题，哈哈哈哈合肥市说u 发挥绥芬河市东方。
+
+
+            </div>
+            <div class="realname">Mike zhang</div>
+            <div class="amount">额外奖励100元</div>
+          </div>
+        </div>
+      </div>
+
+      <div class="askDetail">
+        <div class="buttonAsk" @tap.stop.prevent="$router.push('/ask')"><span>+</span> 付费提问</div>
+      </div>
+
+      <div class="mb70"></div>
     </div>
 
-    <div class="askDetail">
-      <div class="buttonAsk" @tap.stop.prevent="$router.push('/ask')"><span>+</span> 付费提问</div>
-    </div>
 
-    <div class="mb70"></div>
+
   </div>
-  </div>
+
 </template>
 <script>
 
@@ -84,45 +105,51 @@
 
   const Home = {
     data: () => ({
-      loopAsk:false,
-      expertNumber:'--',
-      loading:true,
-      averageAnswerMinute:'--',
-      industryNumber:'--',
-      headerImageUrl:'',
-      recommendQa:[],
-      loading_gif:loading_gif,
-      homeJiangli:homeJiangli,
-      homeJinpai:homeJinpai
+      loopAsk: false,
+      expertNumber: '--',
+      loading: true,
+      averageAnswerMinute: '--',
+      industryNumber: '--',
+      headerImageUrl: '',
+      recommendQa: [],
+      loading_gif: loading_gif,
+      homeJiangli: homeJiangli,
+      homeJinpai: homeJinpai
     }),
     created () {
       this.getData();
     },
-    mounted (){
-        document.body.style.backgroundColor = '#fff';
+    mounted(){
+      document.body.style.backgroundColor = '#fff';
     },
     beforeRouteLeave(to, from, next) {
-        document.body.style.backgroundColor = '#efeff4';
-        next();
+      document.body.style.backgroundColor = '#efeff4';
+      next();
     },
     methods: {
-      getData:function(){
+      toggleMenu(){
+        mui('.mui-off-canvas-wrap').offCanvas('toggle');
+      },
+      expertNav: function () {
+        mui('#expert').popover('toggle');
+      },
+      getData: function () {
         var t = this;
-        apiRequest(`home`,{}).then(response_data => {
+        apiRequest(`home`, {}).then(response_data => {
           t.expertNumber = response_data.expert_number;
           t.averageAnswerMinute = response_data.average_answer_minute;
           t.industryNumber = response_data.industry_number;
-          t.headerImageUrl  = response_data.header_image_url;
-          t.recommendQa  = response_data.recommend_qa;
+          t.headerImageUrl = response_data.header_image_url;
+          t.recommendQa = response_data.recommend_qa;
           t.loading = false;
         });
       },
-      getAsks:function(){
-          this.getData();
-          this.loopAsk = true;
-          setTimeout(() => {
-              this.loopAsk = false;
-          }, 2000)
+      getAsks: function () {
+        this.getData();
+        this.loopAsk = true;
+        setTimeout(() => {
+          this.loopAsk = false;
+        }, 2000)
       }
     }
   };
@@ -130,43 +157,56 @@
 </script>
 
 <style scoped>
-  .professor{
-    background-color:#161616;
-    text-align: center;
-    position:relative;
-    height:136px;
+  .mui-bar .myicon-qian {
+    width: 24px;
+    height: 24px;
+    left: 10px;
+    top: 12px;
   }
 
-  .professor .text{
+  .mui-bar .myicon-expert {
+    width: 28px;
+    height: 28px;
+    right: 10px;
+    top: 12px;
+  }
+
+  .professor {
+    background-color: #161616;
+    text-align: center;
+    position: relative;
+    height: 136px;
+  }
+
+  .professor .text {
     width: 65%;
     height: 100%;
-    padding-top:20px;
-    padding-left:0px;
+    padding-top: 20px;
+    padding-left: 0px;
     display: inline-block;
-    color:#fff;
+    color: #fff;
     text-align: left;
 
   }
 
-  .professor .label{
+  .professor .label {
     position: absolute;
-    right:10px;
-    top:10px;
-    font-size:14px;
+    right: 10px;
+    top: 10px;
+    font-size: 14px;
   }
 
-
-  .professor .text .realname{
-    font-size:20px;
-    font-weight:bolder;
-    margin-left:20px;
+  .professor .text .realname {
+    font-size: 20px;
+    font-weight: bolder;
+    margin-left: 20px;
   }
 
-  .avatar{
+  .avatar {
     z-index: 9;
     color: #ffffff;
     display: inline-block;
-    margin-top:10px;
+    margin-top: 10px;
     height: 100px;
     width: 100px;
     font-size: 20px;
@@ -175,7 +215,7 @@
     vertical-align: top;
   }
 
-  .avatar .avatarInner{
+  .avatar .avatarInner {
     display: -webkit-box;
     display: -webkit-flex;
     display: -ms-flexbox;
@@ -199,144 +239,151 @@
     display: block;
   }
 
-  .professor .options{
+  .professor .options {
     position: relative;
-    margin-top:30px;
-    margin-left:10px;
+    margin-top: 30px;
+    margin-left: 10px;
   }
 
-  .professor .buttonAsk{
+  .professor .buttonAsk {
     position: relative;
     display: inline-block;
-    width:48%;
-    height:37px;
-    font-size:14px;
+    width: 48%;
+    height: 37px;
+    font-size: 14px;
     line-height: 37px;
     text-align: center;
-    border:1px solid #fff;
+    border: 1px solid #fff;
     border-radius: 50px;
-    padding-left:20px;
+    padding-left: 20px;
   }
-  .professor .buttonAsk span{
+
+  .professor .buttonAsk span {
     position: absolute;
     font-size: 30px;
     left: 10px;
     top: -4px;
   }
 
-  .professor .label .collect{
+  .professor .label .collect {
     display: inline-block;
 
-    border:1px solid #86C9EF;
-    border-radius:50%;
-    width:16px;
-    height:16px;
+    border: 1px solid #86C9EF;
+    border-radius: 50%;
+    width: 16px;
+    height: 16px;
     line-height: 16px;
-    color:#161616;
+    color: #161616;
     text-align: center;
-    font-size:12px;
-    margin-right:3px;
+    font-size: 12px;
+    margin-right: 3px;
     background: #86C9EF;
     opacity: 1;
   }
 
-  .professor .options .collect{
+  .professor .options .collect {
     display: inline-block;
-    border:1px solid #fff;
-    border-radius:50%;
-    width:37px;
-    height:37px;
+    border: 1px solid #fff;
+    border-radius: 50%;
+    width: 37px;
+    height: 37px;
     line-height: 37px;
     text-align: center;
-    margin-left:3%;
+    margin-left: 3%;
   }
-  .professorDesc{
-    color:#4A4A4A;
-    font-size:20px;
+
+  .professorDesc {
+    color: #4A4A4A;
+    font-size: 20px;
     background: #fff;
-    padding:10px 20px 10px;
+    padding: 10px 20px 10px;
   }
-  .professorDesc .title{
+
+  .professorDesc .title {
     margin-bottom: 5px;
-    font-size:16px;
+    font-size: 16px;
   }
-  .professorDesc .title .icon{
+
+  .professorDesc .title .icon {
     position: relative;
-    top:3px;
-    width:16px;
-    margin-right:2px;
+    top: 3px;
+    width: 16px;
+    margin-right: 2px;
     display: inline-block;
     background: url("../statics/images/home-jinpai.png") no-repeat;
-    height:16px;
-    background-size:cover;
+    height: 16px;
+    background-size: cover;
   }
-  .professorDesc .detail{
-    font-size:12px;
+
+  .professorDesc .detail {
+    font-size: 12px;
     line-height: 24px;
   }
 
-  .bountyDesc{
+  .bountyDesc {
     background: #fff;
-    margin-top:5px;
-    padding:15px 0 0 20px;
+    margin-top: 5px;
+    padding: 15px 0 0 20px;
   }
 
-  .bountyDesc .title .icon{
-    width:13px;
+  .bountyDesc .title .icon {
+    width: 13px;
     display: inline-block;
     background: url("../statics/images/home-jiangli.png") no-repeat;
-    height:16px;
-    background-size:cover;
+    height: 16px;
+    background-size: cover;
     position: relative;
-    top:2px;
-    margin-right:5px;
+    top: 2px;
+    margin-right: 5px;
   }
 
-  .bountyDesc .title{
+  .bountyDesc .title {
     line-height: 20px;
-    font-size:16px;
-    color:#4A4A4A;
-    border-bottom:1px solid #D9D9D9;
-    padding-bottom:10px;
+    font-size: 16px;
+    color: #4A4A4A;
+    border-bottom: 1px solid #D9D9D9;
+    padding-bottom: 10px;
   }
 
-  .bountyDesc .titleSub{
+  .bountyDesc .titleSub {
     display: inline-block;
-    font-size:12px;
-    color:#9B9B9B;
+    font-size: 12px;
+    color: #9B9B9B;
     position: relative;
-    bottom:1px;
+    bottom: 1px;
   }
 
-  .bountyDesc .more{
+  .bountyDesc .more {
     display: inline-block;
     width: 63px;
-    float:right;
+    float: right;
     position: relative;
   }
 
-  .bountyDescList{
+  .bountyDescList {
     background: #fff;
   }
-  .bountyDescList .detail{
+
+  .bountyDescList .detail {
     position: relative;
     padding: 20px 0 0 20px;
   }
-  .bountyDescList .detail .image{
+
+  .bountyDescList .detail .image {
     position: absolute;
     display: inline-block;
-    width:60px;
+    width: 60px;
   }
 
-  .bountyDescList .detail .image img{
-    width:60px;
-    height:60px;
+  .bountyDescList .detail .image img {
+    width: 60px;
+    height: 60px;
   }
 
-  .bountyDescList .detail .text{
-    margin-left:70px;
-    padding-bottom:40px;
-    padding-right:10px;
+  .bountyDescList .detail .text {
+    margin-left: 70px;
+    padding-bottom: 40px;
+    padding-right: 10px;
     position: relative;
   }
 
@@ -352,81 +399,85 @@
     background-color: #D9D9D9;
   }
 
-  .bountyDescList .detail:last-child .text{
+  .bountyDescList .detail:last-child .text {
     border-bottom: none;
   }
 
-  .bountyDescList .detail .text .content{
-    font-size:12px;
-    padding-right:10px;
-  }
-  .bountyDescList .detail .realname{
-    color:#4990E2;
-    position: absolute;
-    left:0;
-    bottom:10px;
-    font-size:14px;
-  }
-  .bountyDescList .detail .amount{
-    color:#F6A623;
-    font-size:14px;
-    position: absolute;
-    right:20px;
-    bottom:10px;
+  .bountyDescList .detail .text .content {
+    font-size: 12px;
+    padding-right: 10px;
   }
 
-  .askDetail{
-    height:67px;
+  .bountyDescList .detail .realname {
+    color: #4990E2;
+    position: absolute;
+    left: 0;
+    bottom: 10px;
+    font-size: 14px;
+  }
+
+  .bountyDescList .detail .amount {
+    color: #F6A623;
+    font-size: 14px;
+    position: absolute;
+    right: 20px;
+    bottom: 10px;
+  }
+
+  .askDetail {
+    height: 67px;
     line-height: 67px;
     text-align: center;
     background: #fff;
-    margin-top:5px;
+    margin-top: 5px;
   }
-  .askDetail .buttonAsk{
+
+  .askDetail .buttonAsk {
     position: relative;
     background: #4990E2;
     display: inline-block;
-    width:125px;
-    height:37px;
-    font-size:14px;
+    width: 125px;
+    height: 37px;
+    font-size: 14px;
     line-height: 37px;
     text-align: center;
     border-radius: 50px;
-    padding-left:20px;
+    padding-left: 20px;
 
-    color:#fff;
+    color: #fff;
   }
-  .askDetail .buttonAsk span{
+
+  .askDetail .buttonAsk span {
     position: absolute;
     font-size: 30px;
     left: 20px;
     top: -4px;
   }
 
-  .footer-bar{
-    background-color:#fff;
+  .footer-bar {
+    background-color: #fff;
     box-shadow: none;
   }
-  .footer-bar{
+
+  .footer-bar {
     border-top: 1px solid #D9D9D9;
   }
 
-  .footer-bar .mui-active .myicon-point{
-    width:3px;
-    height:3px;
-    top:8px;
-    left:-13px;
+  .footer-bar .mui-active .myicon-point {
+    width: 3px;
+    height: 3px;
+    top: 8px;
+    left: -13px;
   }
 
-  @media (min-device-width : 375px) and (max-device-width : 667px) and (-webkit-min-device-pixel-ratio : 2){
-    .professor .options .collect{
+  @media (min-device-width: 375px) and (max-device-width: 667px) and (-webkit-min-device-pixel-ratio: 2) {
+    .professor .options .collect {
       margin-left: 7% !important;
     }
   }
 
-
-  @media (min-device-width : 414px) and (max-device-width : 736px) and (-webkit-min-device-pixel-ratio : 3){
-    .professor .options .collect{
+  @media (min-device-width: 414px) and (max-device-width: 736px) and (-webkit-min-device-pixel-ratio: 3) {
+    .professor .options .collect {
       margin-left: 9% !important;
     }
   }
