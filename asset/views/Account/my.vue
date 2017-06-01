436094cff399420cfb4c9b449af476f3da32d090
edwin <edwincoder@163.com>
2017-06-01 15:27:15 +0800

    fix #282

--- asset/views/Account/my.vue
+++ asset/views/Account/my.vue
@@ -53,7 +53,7 @@
           </a>
         </li>
         <li class="mui-table-view-cell">
-          <a class="mui-navigate-right"><span class="mui-icon myicon myicon-coupon"></span>优惠券</a>
+          <a class="mui-navigate-right"><span class="mui-icon myicon myicon-coupon"></span>我的邀请码</a>
         </li>
         <li class="mui-table-view-cell">
           <a class="mui-navigate-right"><span class="mui-icon myicon myicon-huiyuan"></span>会员福利</a>
