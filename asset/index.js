4e5bb88c9c107e04ec8a9914da1d2074945ed47d
hank <hank.huiwang@gmail.com>
2017-06-05 11:29:03 +0800

    环境区分

--- asset/index.js
+++ asset/index.js
@@ -20,8 +20,13 @@ import store from './stores/store';
 import App from './App';
 import Raven from 'raven-js';
 import RavenVue from 'raven-js/plugins/vue';
+
+var sentry_url = 'https://6cd9e4811e7f4ade86ff3d4a18b28e19@sentry.io/167478';
+if (process.env.NODE_ENV === 'production') {
+  sentry_url = 'https://0b85668d118943ffa71c7b6c34f0a6bf@sentry.io/175782';
+}
 Raven
-  .config('https://6cd9e4811e7f4ade86ff3d4a18b28e19@sentry.io/167478')
+  .config(sentry_url)
   .addPlugin(RavenVue, Vue)
   .install();
 
--- build/dev-server.js
+++ build/dev-server.js
@@ -62,7 +62,7 @@ app.use(hotMiddleware)
 var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
 
-var uri = 'http://localhost:8080';
+var uri = 'http://localhost:8076';
 
 devMiddleware.waitUntilValid(function () {
   console.log('> Listening at ' + uri + '\n')
