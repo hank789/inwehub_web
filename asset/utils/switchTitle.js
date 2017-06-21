bda7392dca5a7e87d7e879f5fbdbbb98b2a95d87
hank <hank.huiwang@gmail.com>
2017-06-21 17:28:27 +0800

    update

--- asset/utils/switchTitle.js
+++ asset/utils/switchTitle.js
@@ -5,7 +5,7 @@ module.exports = function (title) {
     var iframe = document.createElement('iframe')
     iframe.style.display = 'none'
     // 替换成站标favicon路径或者任意存在的较小的图片即可
-    iframe.setAttribute('src', '../images/favicon.ico')
+    iframe.setAttribute('src', '/favicon.ico')
     var iframeCallback = function () {
       setTimeout(function () {
         iframe.removeEventListener('load', iframeCallback)
