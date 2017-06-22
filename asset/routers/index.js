87777165116f8ea6041eb56a287b9f51d056725a
edwin <edwincoder@163.com>
2017-06-22 17:09:06 +0800

    基础路由删减

--- asset/routers/index.js
+++ asset/routers/index.js
@@ -1,9 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import routes from './routes';
-import {checkUpdate} from '../utils/updateVersion';
-import localEvent from '../stores/localStorage';
 
+var routes = [];
 
 VueRouter.prototype.goBack = function () {
   this.isBack = true;
@@ -14,18 +12,6 @@ Vue.use(VueRouter);
 const router = new VueRouter({
   mode: 'hash',
   routes
-});
-
-router.beforeEach((to, from, next) => {
-  // 检查版本更新
-  mui.plusReady(function() {
-    checkUpdate();
-  });
-
-  var referer = from.path;
-  localEvent.setLocalItem('referer', {path:referer});
-
-  next();
 });
 
 export default router;
