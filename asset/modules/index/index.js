078cfee4ce2470933ddf925f2dff6f4b5cfdd414
edwin <edwincoder@163.com>
2017-06-22 20:00:58 +0800

    添加全局加载动画

--- asset/modules/index/index.js
+++ asset/modules/index/index.js
@@ -42,6 +42,31 @@ Vue.use(VueWechatTitle);
 
 mui.init({swipeBack:true});
 
+mui.waiting = function () {
+
+  var canShutdown = false;
+
+  var mask = mui.createMask(function(){
+    return canShutdown;
+  });
+
+  var element = document.createElement('div');
+  element.innerHTML='<div class="mint-indicator" style="display: block;"><div class="mint-indicator-wrapper" style="padding: 8px 8px 4px 8px;"><div class="mint-indicator-spin"><div class="mint-spinner-fading-circle circle-color-19" style="width: 20px; height: 20px;"><div class="mint-spinner-fading-circle-circle is-circle2"></div><div class="mint-spinner-fading-circle-circle is-circle3"></div><div class="mint-spinner-fading-circle-circle is-circle4"></div><div class="mint-spinner-fading-circle-circle is-circle5"></div><div class="mint-spinner-fading-circle-circle is-circle6"></div><div class="mint-spinner-fading-circle-circle is-circle7"></div><div class="mint-spinner-fading-circle-circle is-circle8"></div><div class="mint-spinner-fading-circle-circle is-circle9"></div><div class="mint-spinner-fading-circle-circle is-circle10"></div><div class="mint-spinner-fading-circle-circle is-circle11"></div><div class="mint-spinner-fading-circle-circle is-circle12"></div><div class="mint-spinner-fading-circle-circle is-circle13"></div></div></div></div></div>';
+  document.body.appendChild(element);
+
+  mui.closeWaiting = function(){
+    canShutdown = true;
+    mask.close();
+    document.body.removeChild(element);
+  }
+
+  mask.show();//显示遮罩
+}
+
+
+
+
+
 Vue.use(TimeAgo, {
   name: 'timeago',
   locale: 'zh-CN',
