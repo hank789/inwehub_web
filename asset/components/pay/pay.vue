07cb9542bf7afa82e70ec71eb3c9ef8b0fe1078c
hank <hank.huiwang@gmail.com>
2017-06-27 16:30:18 +0800

    微信支付

--- asset/components/pay/pay.vue
+++ asset/components/pay/pay.vue
@@ -75,25 +75,19 @@
                 this.requestIapOrder(response_data);
               } else if (id ==='wx_pub') {
                 mui.waiting();
-                alert(response_data.order_info);
                 var wx_order_info = JSON.parse(response_data.order_info);
                 //h5微信支付
                 WeixinJSBridge.invoke(
-                  'getBrandWCPayRequest', {
-                    "appId":wx_order_info.appId,     //公众号名称，由商户传入
-                    "timeStamp":wx_order_info.timeStamp,         //时间戳，自1970年以来的秒数
-                    "nonceStr":wx_order_info.nonceStr, //随机串
-                    "package":wx_order_info.package,
-                    "signType":wx_order_info.signType,         //微信签名方式：
-                    "paySign":wx_order_info.paySign //微信签名
-                  },
+                  'getBrandWCPayRequest', wx_order_info,
                   (res) => {
                     mui.closeWaiting();
-                    if(res.err_msg == "get_brand_wcpay_request:ok" ) {
+                    if(res.err_msg === "get_brand_wcpay_request:ok" ) {
                       this.$emit('pay_success', response_data.order_id, this.pay_object_type);
                     }     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
-                    else{
-                      alert("支付失败，请稍后再试！");
+                    else if (res.err_msg === 'get_brand_wcpay_request:cancel'){
+                      mui.alert("您已取消支付！");
+                    } else {
+                      mui.alert("支付失败，请稍后再试！");
                     }
                   }
                 );
