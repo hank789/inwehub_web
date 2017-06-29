43ad47537aa4514897f7367e89ab80f14aff32e6
edwin <edwincoder@163.com>
2017-06-29 18:25:56 +0800

    通知跳转地址修改

--- asset/modules/index/App.vue
+++ asset/modules/index/App.vue
@@ -151,12 +151,12 @@
               case 'question':
                  // mui.alert('/ask/' + payload.object_id + '?time=' + Date.parse(new Date()));
                 //router.go(-1);
-                router.pushPlus('/ask/' + payload.object_id + '?time=' + Date.parse(new Date()));
+                router.pushPlus('/ask/' + payload.object_id);
                 break;
               case 'answer':
                 //router.go(-1);
                  // mui.alert('/answer/' + payload.object_id + '?time=' + Date.parse(new Date()));
-                router.pushPlus('/answer/' + payload.object_id + '?time=' + Date.parse(new Date()));
+                router.pushPlus('/answer/' + payload.object_id);
                 break;
             }
           };
