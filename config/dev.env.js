727d89bb2cd4dfcb2ed1c8b460cd272268b28617
hank <hank.huiwang@gmail.com>
2017-07-05 10:36:50 +0800

    revert

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,6 +3,6 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ROOT: '"http://zhihu.hank.com:8080/"',
+  API_ROOT: '"https://api.ywhub.com/"',
   GA_CODE: '"UA-97476315-3"'
 })
