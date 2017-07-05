a6520aba614f612b7dfe1728fc182f3930ddee47
edwin <edwincoder@163.com>
2017-07-05 20:19:42 +0800

    分享给朋友添加图标

--- asset/views/Account/Resume.vue
+++ asset/views/Account/Resume.vue
@@ -279,7 +279,7 @@
               title: 'test', // 分享标题
               desc: 'test', // 分享描述
               link: t.shareUrl, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-              imgUrl: '', // 分享图标
+              imgUrl: t.resume.info.avatar_url, // 分享图标
               type: '', // 分享类型,music、video或link，不填默认为link
               dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
               success: function () {
