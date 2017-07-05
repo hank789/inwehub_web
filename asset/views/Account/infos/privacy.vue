df3ae5c217fa47f4de33037b4c13c62102431184
edwin <edwincoder@163.com>
2017-07-05 18:27:18 +0800

    隐私设置添加业务逻辑

--- asset/views/Account/infos/privacy.vue
+++ asset/views/Account/infos/privacy.vue
@@ -6,7 +6,7 @@
       <!--<a class="mui-icon myicon myicon-share mui-pull-right"></a>-->
     </header>
 
-    <div class="mui-content">
+    <div class="mui-content" v-show="!loading">
       <ul class="mui-table-view mui-table-view-chevron">
         <li class="mui-table-view-cell">
           工作经历
@@ -42,39 +42,83 @@
 
   import {NOTICE, TASK_LIST_APPEND, ANSWERS_LIST_APPEND, ASKS_LIST_APPEND, USERS_APPEND} from '../../../stores/types';
   import router from '../../../routers/index';
+  import {createAPI, addAccessToken, postRequest} from '../../../utils/request';
 
 
   export  default {
     data(){
-
       return {
-          jobSee:true,
-          projectSee:true,
-          eduSee:true
+          jobSee:0,
+          projectSee:0,
+          eduSee:0,
+          loading:true
       }
     },
+    created () {
+      postRequest(`profile/privacy/info`, {}).then(response => {
+        var code = response.data.code;
+        if (code !== 1000) {
+          mui.toast(response.data.message);
+        }
+
+        this.jobSee = response.data.data.is_job_info_public;
+        this.projectSee = response.data.data.is_project_info_public;
+        this.eduSee = response.data.data.is_edu_info_public;
+        this.loading = 0;
+      });
+    },
     methods:{
+      submit(data) {
+        postRequest(`profile/privacy/update`, data).then(response => {
+          var code = response.data.code;
+          if (code !== 1000) {
+            mui.toast(response.data.message);
+          }
+
+          mui.toast(response.data.message);
+        });
+      },
       toggle(type){
           switch(type) {
             case 'job':
                 this.jobSee = !this.jobSee;
-                if (!this.jobSee) {
-                    mui.toast('工作经历仅自己可见');
-                }
+//                if (!this.jobSee) {
+//                    mui.toast('工作经历仅自己可见');
+//                }
+                var data = {
+                  is_job_info_public:this.jobSee?1:0,
+                };
+                this.submit(data);
+
                 break;
             case 'project':
               this.projectSee = !this.projectSee;
-              if (!this.projectSee) {
-                mui.toast('项目经历仅自己可见');
-              }
+
+              var data = {
+                is_project_info_public:this.projectSee?1:0,
+              };
+              this.submit(data);
+//              if (!this.projectSee) {
+//                mui.toast('项目经历仅自己可见');
+//              }
                 break;
             case 'edu':
               this.eduSee = !this.eduSee;
-              if (!this.eduSee) {
-                mui.toast('教育经历仅自己可见');
-              }
-                break;
+              var data = {
+                is_edu_info_public:this.eduSee?1:0,
+              };
+              this.submit(data);
+//              if (!this.eduSee) {
+//                mui.toast('教育经历仅自己可见');
+//              }
+              break;
           }
+
+
+
+
+
+
       },
 
     }
