f2279e73b958cb4cf50e5c79f51a499419d2d813
丁冉 <dingran@dingrandeMacBook-Air.local>
2017-07-26 17:45:04 +0800

    add product list

---
+++ asset/views/Project/List.vue
@@ -0,0 +1,165 @@
+<template>
+  <div>
+    <header class="mui-bar mui-bar-dark mui-bar-nav">
+      <a class="mui-action-back mui-icon mui-icon-left-nav mui-pull-left"></a>
+      <h1 class="mui-title">需求管理</h1>
+    </header>
+  <div class="mui-content">
+  	  <ul class="projectList1" v-for="item in list">
+  	  	<li>
+  	  		<span>{{item.project_name}}</span>
+  	  		<span>等待审核</span>
+  	  		<svg class="icon" aria-hidden="true">
+			  <use xlink:href="#icon-shuru"></use>
+			</svg>
+  	  	</li>
+  	  	<li>
+  	  		<span>{{item.company_represent_person_name}}</span>
+  	  		<span></span>
+  	  		<span>{{item.company_name}}</span>
+  	  	</li>
+  	  	<li>{{item.updated_at}}</li>
+  	  </ul>
+  </div>
+   
+   
+    
+</div>
+</template>
+<script>
+	import {apiRequest, postRequest} from '../../utils/request';
+	export default { 
+    data(){
+      return {
+      	 list:[]
+      }
+    },
+    methods: {
+//  	submit(){
+//      postRequest(`project/myList`, data).then(response => {
+//        var code = response.data;
+//        console.log(code)
+//        if (code !== 1000) {
+//          mui.alert(response.data.message);
+//          return;
+//        }
+//      
+//   });
+//  }
+    	},
+    mounted(){
+    	  
+      postRequest("project/myList", {}).then(response => {
+          var code = response.data.code;
+          if (code !== 1000) {
+            mui.alert(response.data.message);
+            return;
+          }
+          
+    	      this.list = response.data.data; 
+    	      console.log(this.list)
+     });
+    }
+    
+	}
+</script>
+
+<style lang="less" rel="stylesheet/less" scoped>
+ ul {
+ 	padding: 0;
+ 	margin: 0;
+ 	li{
+ 		list-style: none;
+ 	}
+ }
+  .mui-content{
+    position: fixed;
+    height:100%;
+    width:100%;
+    background: #ececee; 
+  }
+  .projectList1{
+	width: 100%;
+	background: #ffffff;
+	padding: 17px 16px 16px 16px;
+	margin-bottom: 10px;
+	position: relative;
+	li{
+		&:nth-child(1){
+		width: 309px;
+	  	span{
+	  		&:nth-child(1){
+		  		/*width: 125px;*/
+				height: 22.5px;
+				font-family: "PingFangSC";
+				font-size: 16px;
+				color: #444444;
+			}
+			&:nth-child(2){
+				/*width:65px;*/
+				height: 20px;
+			    border-radius: 50px;
+				background: #a8dff7;
+				font-family: "PingFangSC";
+		        font-size: 11px;
+	            text-align:center;
+	            color: #ffffff;
+	            padding: 2px 11px 3px 10px;
+	            margin-left:8px;
+			}
+			}
+			svg{
+				position:absolute;
+				top: 20px;
+				right: 16px;
+				width: 16px;
+				height: 17px;
+				color: #03aef9;
+			}
+  }
+  &:nth-child(2){
+  	margin-top: 20px;
+  	width: 100%;
+  	height: 14px;
+  	span{
+  	&:nth-child(1){
+  		/*width: 44px;*/
+		height: 14px;
+		font-family: ".PingFangSC";
+		color: #808080;
+  	    font-size: 14px;
+		}
+	&:nth-child(2){
+	display: inline-block;
+	width: 1px;
+	height: 14px;
+	background:#c8c8c8;	
+	margin-left: 8px;
+	margin-right: 8px;
+	margin-bottom: -2px;	
+		
+	}
+  &:nth-child(3){
+   	/*width: 146.5px;*/
+	height: 14px;
+	font-family: ".PingFangSC";
+	color: #808080;
+	color: #808080;
+  	font-size: 14px;
+   }
+  	
+  }
+  
+  }
+  &:nth-child(3){
+  	margin-top: 15.5px;
+  	width:100%;
+	height: 16.5px;
+	font-family: "PingFangSC";
+	font-size: 12px;
+	color: #b4b4b6;
+  }
+  }
+  }
+  
+</style>
