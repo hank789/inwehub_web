9dac92bf60382ba11129a9fcc26e2bbbb2c28e89
edwin <edwincoder@163.com>
2017-07-28 16:05:45 +0800

    发布者本人时，对接人信息不显示输入光标

--- asset/views/Project/Company.vue
+++ asset/views/Project/Company.vue
@@ -66,28 +66,32 @@
           <div class="mui-input-row">
             <label class="mui-navigate">对接人名</label>
             <input type="text" name="company_represent_person_name" v-model="company_represent_person_name" placeholder="输入对接人名" v-if="parseInt(company_represent_person_is_self) === 0 ">
-            <input type="text" name="company_represent_person_name_1" v-model="company_represent_person_name" @tap.stop.prevent="blurThis($event)" readonly="readonly" v-else>
+            <div class="inputOnlyText" v-else>{{ company_represent_person_name }}</div>
+            <!--<input type="text" name="company_represent_person_name_1" v-model="company_represent_person_name" @tap.stop.prevent="blurThis($event)" readonly="readonly" v-else>-->
           </div>
         </li>
         <li class="mui-table-view-cell">
           <div class="mui-input-row">
             <label class="mui-navigate">对接职位</label>
             <input type="text" name="company_represent_person_title" v-model="company_represent_person_title" placeholder="输入对接人职位" v-if="parseInt(company_represent_person_is_self) ===0">
-            <input type="text" name="company_represent_person_title_1" v-model="company_represent_person_title" @tap.stop.prevent="blurThis($event)" readonly="readonly" v-else>
+            <div class="inputOnlyText" v-else>{{ company_represent_person_title }}</div>
+            <!--<input type="text" name="company_represent_person_title_1" v-model="company_represent_person_title" @tap.stop.prevent="blurThis($event)" readonly="readonly" v-else>-->
           </div>
         </li>
         <li class="mui-table-view-cell">
           <div class="mui-input-row">
             <label class="mui-navigate">对接手机</label>
             <input type="text" name="company_represent_person_phone" pattern="\d*" v-model="company_represent_person_phone" placeholder="输入对接人手机" v-if="parseInt(company_represent_person_is_self) === 0 ">
-            <input type="text" name="company_represent_person_phone_1" pattern="\d*" v-model="company_represent_person_phone" @tap.stop.prevent="blurThis($event)" readonly="readonly"  v-else>
+            <div class="inputOnlyText" v-else>{{ company_represent_person_phone }}</div>
+            <!--<input type="text" name="company_represent_person_phone_1" pattern="\d*" v-model="company_represent_person_phone" @tap.stop.prevent="blurThis($event)" readonly="readonly"  v-else>-->
           </div>
         </li>
         <li class="mui-table-view-cell">
           <div class="mui-input-row">
             <label class="mui-navigate">对接邮箱</label>
             <input type="text" name="company_represent_person_email" v-model="company_represent_person_email" placeholder="输入对接人邮箱" v-if="parseInt(company_represent_person_is_self) === 0">
-            <input type="text" name="company_represent_person_email1" v-model="company_represent_person_email" @tap.stop.prevent="blurThis($event)" readonly="readonly" v-else>
+            <div class="inputOnlyText" v-else>{{ company_represent_person_email }}</div>
+            <!--<input type="text" name="company_represent_person_email1" v-model="company_represent_person_email" @tap.stop.prevent="blurThis($event)" readonly="readonly" v-else>-->
           </div>
         </li>
 
@@ -523,6 +527,17 @@
     font-size:14px;
     padding-right: 0;
 
+  }
+
+  .companyForm .inputOnlyText{
+    width:61% !important;
+    text-align: right;
+    font-size:14px;
+    float: right;
+    border: 0;
+    padding: 10px 0;
+    height:40px;
+    line-height: 21px;
   }
 
   .companyForm .mui-table-view-cell{
