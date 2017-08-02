0347662a8d6459a8858b3bdd542a050f88b1b6bb
edwin <edwincoder@163.com>
2017-08-02 16:48:13 +0800

    修改waiting

--- asset/modules/index/index.js
+++ asset/modules/index/index.js
@@ -52,37 +52,8 @@ Vue.use(VueWechatTitle);
 //   try: 3
 // });
 
-var waitingElements = [];
-mui.waiting = function () {
-
-  //var canShutdown = false;
-
-  // var mask = mui.createMask(function(){
-  //   return canShutdown;
-  // });
-
-  var indicator = document.getElementsByClassName('mint-indicator');
-  if (indicator.length) {
-    return;
-  }
-
-  var element = document.createElement('div');
-  element.innerHTML='<div class="mint-indicator" style="display: block;"><div class="mint-indicator-wrapper" style="padding: 8px 8px 4px 8px;"><div class="mint-indicator-spin"><div class="mint-spinner-fading-circle circle-color-19" style="width: 20px; height: 20px;"><div class="mint-spinner-fading-circle-circle is-circle2"></div><div class="mint-spinner-fading-circle-circle is-circle3"></div><div class="mint-spinner-fading-circle-circle is-circle4"></div><div class="mint-spinner-fading-circle-circle is-circle5"></div><div class="mint-spinner-fading-circle-circle is-circle6"></div><div class="mint-spinner-fading-circle-circle is-circle7"></div><div class="mint-spinner-fading-circle-circle is-circle8"></div><div class="mint-spinner-fading-circle-circle is-circle9"></div><div class="mint-spinner-fading-circle-circle is-circle10"></div><div class="mint-spinner-fading-circle-circle is-circle11"></div><div class="mint-spinner-fading-circle-circle is-circle12"></div><div class="mint-spinner-fading-circle-circle is-circle13"></div></div></div></div><div class="mint-indicator-mask"></div></div>';
-  document.body.appendChild(element);
-
-  waitingElements.push(element);
-
-  mui.closeWaiting = function(){
-    var element = waitingElements.pop();
-    if (element) {
-      //mask.close();
-      document.body.removeChild(element);
-    }
-  }
-
-  //mask.show();//显示遮罩
-}
-
+import {bindWaitting} from '../../utils/waiting';
+bindWaitting(mui);
 
 import {bindUploadWaiting} from '../../utils/uploadProgress';
 bindUploadWaiting(mui);
