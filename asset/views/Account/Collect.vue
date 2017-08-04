dab82587dcad9732c9ad7dfc7609985279f326a1
丁冉 <m17621626160@163.com>
2017-08-04 17:00:03 +0800

    我的关注和关注我的

--- asset/views/Account/Collect.vue
+++ asset/views/Account/Collect.vue
@@ -9,26 +9,25 @@
 
     <div class="mui-content mui-scroll-wrapper task-list" id="pullrefresh">
       <div class="mui-scroll">
-        <ul class="mui-table-view mui-table-view-chevron">
-          <li class="mui-table-view-cell" v-for="(item, index) in list">
-            <div class="person">
-              <div class="avatar" @tap.stop.prevent="$router.pushPlus('/share/resume?id=' + item.uuid + '&goback=1')">
-                <div class="avatarInner">
-                  <img :src="item.user_avatar_url" class="avatar"/>
-                </div>
+        <ul class="my-focus">
+          <li class="my-focus-item" v-for="(item, index) in list" >
+              <img :src="item.user_avatar_url"  @tap.stop.prevent="$router.pushPlus('/share/resume?id=' + item.uuid + '&goback=1')" />
+              <div>
+              	<p>
+              		<span>{{item.user_name}}</span>
+              		 <svg  class="icon" aria-hidden="true">
+					    <use xlink:href="#icon-zhuanjiabiaoji"></use>
+				    	 </svg>
+              	</p>
+              	<p>
+              	  <span>{{ item.description }}</span>
+              	</p>
               </div>
-              <div class="mui-media-body">
-                <span class="username">{{ item.user_name }}</span>
-                <div class="site-desc mui-ellipsis-3">
-                  {{ item.description }}&nbsp;
-
-
-                </div>
-              </div>
-              <span class="mui-icon myicon myicon-ask"
-                    @tap.stop.prevent="$router.pushPlus('/ask?id=' + item.uuid)"></span>
-            </div>
-
+              <svg class="icon" aria-hidden="true"   @tap.stop.prevent="$router.pushPlus('/ask?id=' + item.uuid)" >
+			    <use xlink:href="#tiwen"></use>
+		    	 </svg>
+		    	 
+		    	 <i class="bot"></i>
           </li>
 
 
@@ -149,149 +148,84 @@
 </script>
 
 <style scoped>
-
-  .menu {
-    position: relative;
-  }
-
-  .menu:after {
-    position: absolute;
+ 
+  .bot{
+ 	position: absolute;
     right: 0;
     bottom: 0;
-    left: 0;
+    left: 0px;
     height: 1px;
-    content: '';
     -webkit-transform: scaleY(.5);
     transform: scaleY(.5);
-    background-color: #D9D9D9;
-  }
+    background-color: rgb(220,220,220);
+ 	
+ }
+ p {
+ 	margin: 0;
+ 	padding: 0;
+ }
+ .mui-content{
+ 	background: #FFFFFF;
+ }
+ 
+ .my-focus{
+ 	margin: 0;
+ 	padding: 0;
+ 	list-style: none;
+    padding-left:17px;
+    padding-right: 17px;
+ }
+ .my-focus-item{
+ 	width: 100%;
+ 	height:63px;
+ 	list-style: none;
+ 	padding-top: 10px;
+ 	padding-bottom: 10px;
+ 	position: relative;
+ 	
+ }
+ .my-focus-item img{
+ 	width: 44px;
+ 	height: 44px;
+ 	border-radius: 50%;
+ 	margin-right: 8px;
+ 	float: left;
+ }
+ .my-focus-item div{
+ 	
+ 	float: left;
+ }
+ .my-focus-item>svg{
+    font-size: 25px;
+    margin-top: 10px;
+    float: right;
+ }
+ .my-focus-item div p:nth-of-type(1) span{
+    font-family: "PingFangSC";
+	font-size: 14px;
+	color: #444444;
+ 	
+ }
+ 
+ .my-focus-item div p:nth-of-type(1) svg{
+ 	font-size:20px;
+ 	margin-bottom: -2px;
+ 	color: #3c95f9;
 
-  .mui-segmented-control .mui-control-item {
-    line-height: 50px;
-    font-size: 16px;
-  }
+ }
+ 
+ .my-focus-item div p:nth-of-type(2) span{
+ 	font-family: "PingFangSC";
+	font-size:13px;
+	color: #b4b4b6;
+ }
 
-  .mui-segmented-control.mui-segmented-control-inverted .mui-control-item.mui-active {
-    position: relative;
-    border: none;
-  }
-
-  .mui-segmented-control.mui-segmented-control-inverted .mui-control-item.mui-active:after {
-    position: absolute;
-    width: 50px;
-    right: 10px;
-    bottom: 0;
-    left: 50%;
-    margin-left: -25px;
-    height: 5px;
-    content: '';
-    -webkit-transform: scaleY(.5);
-    transform: scaleY(.5);
-    background-color: #009FE8;
-  }
-
-  .task-list {
-    line-height: 33px;
-  }
-
-  .task-list .mui-table-view-chevron .mui-table-view-cell {
-    padding-right: 5px;
-  }
-
-  .task-list .time {
-    display: inline-block;
-    width: 130px;
-    color: #101010;
-  }
-
-  .mui-badge {
-    padding: 5px 10px;
-    position: relative;
-    bottom: 3px;
-  }
-
-  .task-list .link a {
-    color: #8f8f94;
-    font-size: 14px;
-  }
-
-  .task-list .type {
-    font-weight: bold;
-  }
-
-  .mui-media-body {
-    padding-left: 10px;
-  }
-
-  .task-list .username {
-    color: #555555;
-  }
-
-  .mui-media-body {
-    position: relative;
-  }
-
-  .mui-navigate-right:after {
-    font-size: 24px;
-    font-weight: bolder;
-  }
-
-  .avatar {
-    z-index: 0;
-    margin-top: 5px;
-    color: #ffffff;
-    float: left;
-    display: inline-block;
-    height: 55px;
-    width: 55px;
-    font-size: 20px;
-    text-align: center;
-    border-radius: 50%;
-  }
-
-  .avatar .avatarInner {
-    display: -webkit-box;
-    display: -webkit-flex;
-    display: -ms-flexbox;
-    display: flex;
-    width: 100%;
-    height: 100%;
-    -webkit-box-align: center;
-    -webkit-align-items: center;
-    -ms-flex-align: center;
-    align-items: center;
-    -webkit-box-pack: center;
-    -webkit-justify-content: center;
-    -ms-flex-pack: center;
-    justify-content: center;
-  }
-
-  .avatar img {
-    border-radius: 50%;
-    width: 100%;
-    height: 100%;
-    display: block;
-  }
-
-  .site-desc {
-    color: #666;
-    font-size: 12px;
-    line-height: 18px;
-    padding-right: 50px;
-  }
-
-
-
-  .myicon-ask {
-    width: 30px;
-    height: 30px;
-    position: absolute;
-    right: 10px;
-    top: 50%;
-    margin-top: -15px;
-  }
-
-  .person {
-    position: relative;
-  }
+ .my-focus-item div p:nth-of-type(2) i{
+ 	display: inline-block;
+ 	width: 1px;
+ 	height: 12px;
+ 	background:#b4b4b6;
+ 	margin-bottom: -2px;
+ }
+ 
 </style>
