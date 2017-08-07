aa9fa6d490a15c8fbc9a440169e651a0babbd11d
edwin <edwincoder@163.com>
2017-08-07 15:25:12 +0800

    添加Event事件管理器

--- asset/utils/event.js
+++ asset/utils/event.js
@@ -1,14 +1,41 @@
-function addEventListener(name, callback)
-{
-  var listener = function (e) {
-    var result = callback(e);
-    window.removeEventListener(name, listener, false);
-    return result;
-  };
+/**
+ * 事件管理
+ * @type {{events: Array, addEventListener: ((p1?:*, p2?:*)), addOnceEventListener: ((p1?:*, p2:*))}}
+ */
+var EventObj = () => {
+   var events = [];
+   var addEventListener = (name, callback) => {
 
-  window.addEventListener(name, listener, false);
-}
+       if (events[name] !== undefined) {
+           return -1;
+       } else {
 
-export {
-  addEventListener
+           events[name] = callback;
+
+           window.addEventListener(name, callback, false);
+       }
+   };
+
+   var addOnceEventListener = (name, callback) => {
+       if (events[name]) {
+         return -1;
+       } else {
+         var listener = function (e) {
+           var result = callback(e);
+           window.removeEventListener(name, listener, false);
+           return result;
+         };
+
+         window.addEventListener(name, listener, false);
+       }
+   };
+
+   return {
+     addEventListener:addEventListener,
+     addOnceEventListener:addOnceEventListener,
+   };
 };
+
+
+
+export default EventObj();
