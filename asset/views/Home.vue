cef4ff8fd04bd208b9f422f3f4565febbf17012a
丁冉 <m17621626160@163.com>
2017-08-09 17:59:27 +0800

    增加的下载app的布局和样式（隐藏状态）

--- asset/views/Home.vue
+++ asset/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-
+        
 		<header class="hidewechattitle">	
 			<svg class="icon" aria-hidden="true">
 			  <use xlink:href="#icon-logowenzi"></use>
@@ -110,7 +110,34 @@
 				</ul>
 			</div>
              
-             
+             <!--手机端-->
+             <div class="suspend">
+	            <p>
+	            	    <svg class="icon" aria-hidden="true">
+					  <use xlink:href="#icon-logotuxing"></use>
+				    </svg>
+	            </p>
+	            <p>
+	            	   <span>下载APP</span>
+	            	   <span>查看更多专家信息</span>
+	            </p>
+	            <p>
+	            	  立即打开
+	            </p>
+	        </div>
+           <!---->
+           <!--微信端-->
+           <div class="suspension">
+           	   <p>
+           	   	  <svg class="icon" aria-hidden="true">
+					  <use xlink:href="#icon-logotuxing"></use>
+				    </svg>
+           	   </p>
+           	   <p>下载APP</p>
+           	   <p>立即打开</p>
+           </div>
+           
+           
 			<div class="home-bot">
 				你已经到达我的底线
 			</div>
@@ -331,6 +358,111 @@
 </script>
 
 <style lang="less" scoped>
+/*手机端*/
+.suspend{
+	width: 100%;
+	height:64px;
+	border-radius:4px;
+	background:#252525;
+	position: absolute;
+	top:44px;
+	z-index: 999;
+	display: none;
+}
+.suspend p:nth-of-type(1){
+	float: left;
+	width: 47px;
+	height: 47px;
+	border-radius: 12px;
+	background: #F3F4F6;
+	margin-top: 8.5px;
+	margin-left: 10px;
+	text-align: center;	
+}
+.suspend p:nth-of-type(1)>svg{
+	font-size: 34px;
+	margin-top: 6px;
+	color: #009fe8;
+}
+.suspend p:nth-of-type(2){
+	width: 96px;
+	height: 47px;
+	margin-top: 8.5px;
+	margin-left: 10px;
+	padding-top: 5px;
+	float: left;
+}
+.suspend p:nth-of-type(2)>span{
+	display: block;
+	color: #FFFFFF;
+	font-size: 12px;
+}
+.suspend p:nth-of-type(2)>span:nth-of-type(1){
+	font-size:16px
+}
+.suspend p:nth-of-type(2)>span:nth-of-type(2){
+	margin-top: -3px;
+}
+.suspend p:nth-of-type(3){
+	float:right;
+	width: 76px;
+	height:30px;
+	border-radius:4px;
+	background:#3c95f9;
+	text-align: center;
+	line-height: 30px;
+	color: #FFFFFF;
+	margin-top: 17px;
+	margin-right: 10.5px;
+	}
+/*微信端*/
+.suspension{
+	width: 100%;
+	height: 49px;
+	position: absolute;
+    top: 44px;
+    background: #FFFFFF;
+    z-index: 999;
+    box-shadow:0px 3px 5px #b4b4b6;
+    -webkit-box-shadow:0px 3px 5px #b4b4b6;
+    -moz-box-shadow:0px 3px 5px #b4b4b6;
+    display: none;
+}
+.suspension p:nth-of-type(1){
+	float: left;
+	width:35px;
+	height:35px;
+	border-radius:9px;
+	background: #f3f4f6;
+	margin-top: 7px;
+	margin-left: 16px;
+	text-align: center;
+}
+.suspension p:nth-of-type(1) svg{
+	font-size: 25px;
+	margin-top: 6px;
+	color: #009fe8;
+}
+.suspension p:nth-of-type(2){
+    float: left;
+    font-size:16px;
+	color:#171616;
+	margin-top: 14px;
+	margin-left: 10px;
+}
+.suspension p:nth-of-type(3){
+	float:right;
+	width:76px;
+	height:30px;
+	border-radius:4px;
+	background: #3c95f9;
+	font-size: 14px;
+	color: #FFFFFF;
+	text-align: center;
+    line-height: 30px;
+    margin-top: 10px;
+    margin-right: 14px;
+}
 	/*2.0版本css样式*/
 	
 	.mui-content {
