cafefa076cb333f21c57579f32583ca58e27d146
hank <hank.huiwang@gmail.com>
2017-08-14 21:01:58 +0800

    maxpanel

--- asset/modules/index/routers/index.js
+++ asset/modules/index/routers/index.js
@@ -7,6 +7,7 @@ import {openWebviewByUrl} from '../../../utils/webview';
 
 //统计用户的浏览行为;
 import ga from 'vue-ga';
+import VueMultianalytics from 'vue-multianalytics';
 
 VueRouter.prototype.goBack = function () {
   this.isBack = true;
@@ -28,9 +29,25 @@ const gaCode = process.env.GA_CODE;
 
 ga(router, gaCode);
 
+let mixpanelConfig = {
+  token: '688ee16000ddf4f44891e06b79847d4e'
+}
+Vue.use(VueMultianalytics, {
+  modules: {
+    mixpanel: mixpanelConfig
+  },
+  routing: {
+    vueRouter: router, //  Pass the router instance to automatically sync with router (optional)
+    preferredProperty: 'path', // By default 'path' and related with vueRouter (optional)
+    ingoredViews: [], // Views that will not be tracked
+    ignoredModules: ['ga'] // Modules that will not send route change events. The event sent will be this.$ma.trackView({viewName: 'homepage'}, ['ga'])
+  }
+})
+
 router.pushPlus = function (url, autoShow=true, aniShow='pop-in', popGesture='hide', forceWebView = false) {
   console.log('url:'+url);
   if (mui.os.plus && (mui.os.ios || forceWebView)) {
+    Vue.$ma.trackEvent({category: 'Page Viewed', action: url},['ga']);
     openWebviewByUrl(url, autoShow, aniShow, popGesture);
   } else {
     router.push(url);
