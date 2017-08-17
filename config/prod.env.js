e4d2e0c7e1652010a04806df0d047a767c544fe1
hank <hank.huiwang@gmail.com>
2017-08-17 19:46:39 +0800

    fix

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,7 +2,7 @@ module.exports = {
   NODE_ENV: '"production"',
   API_ROOT: '"https://api.inwehub.com/"',
   H5_ROOT: '"https://m.inwehub.com"',
-  READHUB_URL: '"https://read.invehub.com"',
+  READHUB_URL: '"https://read.inwehub.com"',
   GA_CODE: '"UA-97476315-1"',
-  ECHO_SERVER_ADDRESS: '"https://read.invehub.com"'
+  ECHO_SERVER_ADDRESS: '"https://read.inwehub.com"'
 }
