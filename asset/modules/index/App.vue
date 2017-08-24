0c7d0806ad64358e4e3f4dd7ffe96ab489c4f7d3
hank <hank.huiwang@gmail.com>
2017-08-24 20:37:33 +0800

    优化事件监听

--- asset/modules/index/App.vue
+++ asset/modules/index/App.vue
@@ -66,39 +66,35 @@
     mounted () {
       console.log('refreshDataAppMounted');
       var currentUser = localEvent.getLocalItem('UserInfo');
-      if (mui.os.plus) {
-        if (currentUser.uuid) {
-          var url = process.env.READHUB_URL + '/h5?uuid=' + currentUser.uuid;
-          //通过mui.preload()方法预加载，可立即返回对应webview的引用，但一次仅能预加载一个页面；若需加载多个webview，则需多次调用mui.preload()方法；
-          mui.preload({
-            url: url,
-            id: 'inwehub_embed',
-            styles: {
-              popGesture: 'hide',
-              top: '0px',
-              dock: 'top',
-              bottom: '75px',
-              bounce:'none'},
-            extras: {}
-          });
-        }
-        mui.init({
-          swipeBack:true, //启用右滑关闭功能
-          beforeback: goBack
-        });
-      } else {
-        mui.init({
-          swipeBack:true, //启用右滑关闭功能
-        });
-      }
-
       var router = this.$router;
 
       mui.plusReady(function () {
         if (mui.os.plus) {
+          if (currentUser.uuid) {
+            var url = process.env.READHUB_URL + '/h5?uuid=' + currentUser.uuid;
+            //通过mui.preload()方法预加载，可立即返回对应webview的引用，但一次仅能预加载一个页面；若需加载多个webview，则需多次调用mui.preload()方法；
+            mui.preload({
+              url: url,
+              id: 'inwehub_embed',
+              styles: {
+                popGesture: 'hide',
+                top: '0px',
+                dock: 'top',
+                bottom: '75px',
+                bounce:'none'},
+              extras: {}
+            });
+          }
+          mui.init({
+            swipeBack:true, //启用右滑关闭功能
+            beforeback: goBack
+          });
+
+
           var ws = plus.webview.currentWebview();
           console.log('bindEvent-runtime:' + plus.runtime.appid);
           console.log('bindEvent-wsid:' + ws.id);
+          // 只在主页面监听一次
           if (ws.id === plus.runtime.appid) {
             EventObj.addEventListener('refreshData', (e) => {
               //执行刷新
@@ -229,7 +225,10 @@
 
             }, false);
           }
-
+        } else {
+          mui.init({
+            swipeBack:true, //启用右滑关闭功能
+          });
         }
       });
     }
