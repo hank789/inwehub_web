7a3657ef5e78c5d3e4153a7c8c670fc86ff69880
hank <hank.huiwang@gmail.com>
2017-09-18 17:29:34 +0800

    优化

--- asset/views/Home.vue
+++ asset/views/Home.vue
@@ -439,15 +439,15 @@
 
 					//推荐专家；
 				   t.recommend_experts = response_data.recommend_experts;
-				   
+
 				   this.isShowActivity = response_data.recommend_activity.length?true:false;
-				   
+
 				   //首页推荐活动
 				   for (var i in response_data.recommend_activity) {
 				   	   t.recommend_activity[i] = response_data.recommend_activity[i];
 				   }
-				   
-				   
+
+
 				   //推荐阅读；
 				   t.recommend_read = response_data.recommend_read;
 					//返回是否显示首次提问免费的福利；
@@ -455,8 +455,26 @@
 					//是否是专家；
 					t.is_expert = response_data.expert_apply_status;
 
-
-
+          //预加载第一篇文章
+          if (mui.os.plus && t.recommend_read.length > 0) {
+            var article_params = {
+              article_id: t.recommend_read[0].id,
+              article_url: t.recommend_read[0].view_url,
+              article_title: t.recommend_read[0].title,
+              article_comment_url: t.recommend_read[0].comment_url,
+              article_img_url:t.recommend_read[0].img_url,
+              preload: true,
+              custom_preload: true
+            };
+            mui.preload({
+              url: 'index.html#/webview/article',
+              id: 'inwehub_article_view',
+              styles: {
+                popGesture: 'hide'
+              },
+              extras: article_params
+            });
+          }
 
 
 
