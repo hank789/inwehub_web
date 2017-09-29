98e7c28113a69701364736a6b2e478e081ffa5b1
hank <hank.huiwang@gmail.com>
2017-09-29 11:32:57 +0800

    优化社区问答列表页面

--- asset/views/Ask/AskDetail.vue
+++ asset/views/Ask/AskDetail.vue
@@ -120,7 +120,7 @@
           var code = response.data.code;
           if (code !== 1000) {
             mui.toast(response.data.message);
-            this.$router.pushPlus('/task', true, 'pop-in', 'hide', true);
+            this.$router.pushPlus('/task','' ,true, 'pop-in', 'hide', true);
           }
 
           this.ask = response.data.data;
