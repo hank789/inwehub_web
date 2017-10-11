4a27a44791e4464d88414332e707072989d4caba
edwin <edwincoder@163.com>
2017-10-11 15:57:10 +0800

    添加empty组件

---
+++ asset/components/Empty.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="emptyWrapper">
+        <svg class="icon" aria-hidden="true">
+            <use xlink:href="#icon-zanwushuju"></use>
+        </svg>
+        <div class="description">{{description}}</div>
+    </div>
+</template>
+
+<script type="text/javascript">
+
+    export default {
+        data () {
+            return {}
+        },
+        components: {
+
+        },
+        props: {
+            description: {
+                type: String,
+                default: '暂时还没有数据呀～'
+            },
+        }
+    };
+</script>
+
+<style lang="less" scoped>
+    .emptyWrapper {
+        padding-top:30px;
+        font-size: 50px;
+        text-align: center;
+        color:#dcdcdc;
+
+        .description{
+            margin-top:5px;
+            font-size:12px;
+            color:#c8c8c8;
+        }
+    }
+</style>
