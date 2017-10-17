3662952ecbd22145b2b2778c974d15f9ae7429a1
hank <hank.huiwang@gmail.com>
2017-10-17 16:12:15 +0800

    优化

--- asset/modules/index/index.js
+++ asset/modules/index/index.js
@@ -228,10 +228,8 @@ mui.back = function(){
     if (need_hide.indexOf(current_webview.id) !== -1) {
       goBack();
       current_webview.hide();
-    } else if (mui.os.ios){
+    } else{
       mui.muiOldBack();
-    } else {
-      router.go(-1);
     }
   } else {
     router.go(-1);
