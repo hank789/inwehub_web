a8fd2f26819e8cbe184db2299d658e0811229504
丁冉 <m17621626160@163.com>
2017-10-18 14:31:20 +0800

    数字角标

--- asset/views/Task/InformList.vue
+++ asset/views/Task/InformList.vue
@@ -13,6 +13,7 @@
 					<div class="menu">
 						<span @tap.stop.prevent="$router.replace('/task')">任务</span>
 						<span @tap.stop.prevent="">消息</span>
+						<div class="menu_message" v-if="total_count != 0">{{total_count}}</div>
 						<i></i>
 					</div>
 
@@ -100,7 +101,8 @@
 			task_count: 0,
 			readhub_count: 0,
 			money_count: 0,
-			loading: true
+			loading: true,
+			total_count:0
 		}),
 		methods: {
 			skip(num) {
@@ -151,6 +153,7 @@
 					this.task_count = this.task_message.unread_count;
 					this.readhub_count = this.readhub_message.unread_count;
 					this.money_count = this.money_message.unread_count;
+					this.total_count =this.notice_count+this.task_count+this.readhub_count+this.money_count;
 					//	console.log(this.notice_message)；
 					this.loading = 0;
 					mui('#pullrefresh').pullRefresh().endPulldownToRefresh(); //refresh completed
@@ -319,5 +322,20 @@
 		line-height: 18px;
 		left: 45px;
 		top: 5px;
+	}
+	
+	.menu_message{
+	  position: absolute;
+	  right: 70px;
+    top: 8px;
+	  background: #f03c69;
+	  border-radius: 50%;
+	  min-width: 18px;
+    height: 18px;
+    color: #ffffff;
+    font-size: 11px;
+    text-align: center;
+    line-height: 18px;
+	  padding: 1px;
 	}
 </style>
