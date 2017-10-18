73384dcd77ec3b71cf7f8e2dfe1aa3521e3411ed
丁冉 <m17621626160@163.com>
2017-10-18 11:57:58 +0800

    answer

--- asset/components/Footer.vue
+++ asset/components/Footer.vue
@@ -134,7 +134,7 @@
           //1为专业问答  2为互动问答
           if(ask.question.question_type == 1) {
             this.shareoption.shareTitle = '专家回答|' + ask.question.description;
-            var currentUrl = '/askCommunity/major/' + ask.question.id;
+            var currentUrl = '/answer/' + ask.question.id;
             this.shareoption.shareUrl = process.env.API_ROOT + 'wechat/oauth?redirect=' + currentUrl;
             var answername = ask.question.user_name;
             this.shareoption.shareContent = '专家' + answername + '的回答，' + '点击前往围观互动';
