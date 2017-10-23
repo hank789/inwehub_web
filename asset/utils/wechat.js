18dd493b1cfcb5c7477f3003705cdd508950cb67
丁冉 <m17621626160@163.com>
2017-10-23 15:48:10 +0800

    覆盖

--- asset/utils/wechat.js
+++ asset/utils/wechat.js
@@ -36,7 +36,7 @@ function rebootAuth(hash)
        hash = window.location.hash.replace('#', '');
     }
     var url = process.env.API_ROOT + 'wechat/oauth?redirect=' + hash;
-    //window.location.href = url;
+      window.location.href = url;
   }
 }
 
