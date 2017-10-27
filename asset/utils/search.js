628a2f52e351fb21217ffb1e5833ad1afb2e4d4d
edwin <edwincoder@163.com>
2017-10-27 19:41:10 +0800

    添加搜索接口公共方法

---
+++ asset/utils/search.js
@@ -0,0 +1,27 @@
+/**
+ * 通过接口搜索
+ * @param text  要搜索的文本
+ * @param callback  执行搜索的操作
+ */
+
+var searchTimer = null;
+
+function searchText(text, callback) {
+  if (!text) {
+    return;
+  }
+
+  if (searchTimer) {
+     clearTimeout(searchTimer);
+  }
+
+  searchTimer = setTimeout(() => {
+    console.log('search:' + text);
+    callback();
+  }, 1000);
+}
+
+
+export {
+  searchText
+}
