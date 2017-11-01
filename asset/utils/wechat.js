2523da1af7cba3fcb07e3bc07744bc16acf55604
edwin <edwincoder@163.com>
2017-11-01 11:41:07 +0800

    utils code format

--- asset/utils/wechat.js
+++ asset/utils/wechat.js
@@ -1,27 +1,25 @@
-function hideHeaderHandler(obj, type){
-  if (mui.os.wechat) {
-
+function hideHeaderHandler (obj, type) {
+  if (window.mui.os.wechat) {
     if (obj.$router.currentRoute.meta.keepAlive && type === 'mounted') {
-      return;
+      return
     }
 
-    var headers = document.getElementsByTagName('header');
+    var headers = document.getElementsByTagName('header')
     for (var i in headers) {
-      var destHeader = headers[i];
+      var destHeader = headers[i]
       if (obj.$router.currentRoute.meta.wechatHideHeader) {
         if (destHeader && destHeader.className) {
           if (!/hidden/.test(destHeader.className) && !/mustshow/.test(destHeader.className)) {
-            destHeader.className = destHeader.className.replace("mui-bar-nav", "");
-            destHeader.className = destHeader.className.replace("mui-bar", "");
-            destHeader.className += " mui-wechat-hidden";
+            destHeader.className = destHeader.className.replace('mui-bar-nav', '')
+            destHeader.className = destHeader.className.replace('mui-bar', '')
+            destHeader.className += ' mui-wechat-hidden'
           }
         }
-
       } else {
         if (destHeader && destHeader.className) {
           if (/hidden/.test(destHeader.className)) {
-            destHeader.className = destHeader.className.replace("mui-wechat-hidden", "");
-            destHeader.className += " mui-bar mui-bar-nav";
+            destHeader.className = destHeader.className.replace('mui-wechat-hidden', '')
+            destHeader.className += ' mui-bar mui-bar-nav'
           }
         }
       }
@@ -29,19 +27,18 @@ function hideHeaderHandler(obj, type){
   }
 }
 
-function rebootAuth(hash)
-{
-  if (mui.os.wechat) {
+function rebootAuth (hash) {
+  if (window.mui.os.wechat) {
     if (!hash) {
-       hash = window.location.hash.replace('#', '');
+      hash = window.location.hash.replace('#', '')
     }
-    var url = process.env.API_ROOT + 'wechat/oauth?redirect=' + hash;
-    //-----
-      window.location.href = url;
+    var url = process.env.API_ROOT + 'wechat/oauth?redirect=' + hash
+    // -----
+    window.location.href = url
   }
 }
 
 export {
   hideHeaderHandler,
   rebootAuth
-};
+}
