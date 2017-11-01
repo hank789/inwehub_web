e00552bfb3102d6ab8fa604ef50b5f20d59d0455
edwin <edwincoder@163.com>
2017-11-01 11:13:11 +0800

    code formated

--- asset/utils/select.js
+++ asset/utils/select.js
@@ -1,19 +1,16 @@
-import cityData from '../components/city/city.data';
-
 /**
  * 选择日期时间
  * @param options
  * @param callback
  */
 function selectTime (options, callback) {
-  let picker = new mui.DtPicker(options);
+  let picker = new window.mui.DtPicker(options)
 
   picker.show((rs) => {
-    callback(rs.text);
-    picker.dispose();
-  });
+    callback(rs.text)
+    picker.dispose()
+  })
 }
-
 
 /**
  * 选择key:value
@@ -21,43 +18,41 @@ function selectTime (options, callback) {
  * @param options
  * @param callback (value, key)
  */
-//调用系统的选择框；
-function selectKeyValue(defaultValue, options, callback) {
-
-  var Picker = new mui.PopPicker();
-  Picker.setData(options);
+// 调用系统的选择框；
+function selectKeyValue (defaultValue, options, callback) {
+  var Picker = new window.mui.PopPicker()
+  Picker.setData(options)
 
   if (defaultValue) {
-    Picker.pickers[0].setSelectedValue(defaultValue);
+    Picker.pickers[0].setSelectedValue(defaultValue)
   }
 
   Picker.show(items => {
-    callback(items[0].value, items[0].text);
-    Picker.dispose();
-  });
+    callback(items[0].value, items[0].text)
+    Picker.dispose()
+  })
 }
 
-function selectCityThreeLevel(defaultValues, data, callback)
-{
-  var cityPicker = new mui.PopPicker({
+function selectCityThreeLevel (defaultValues, data, callback) {
+  var cityPicker = new window.mui.PopPicker({
     layer: 3
-  });
+  })
 
-  cityPicker.setData(data);
+  cityPicker.setData(data)
 
   if (defaultValues) {
     cityPicker.pickers[0].setSelectedValue(defaultValues[0], 0, () => {
       cityPicker.pickers[1].setSelectedValue(defaultValues[1], 0, () => {
         cityPicker.pickers[2].setSelectedValue(defaultValues[2], 0, () => {
-        });
-      });
-    });
+        })
+      })
+    })
   }
 
   cityPicker.show(items => {
-    callback(items);
-    cityPicker.dispose();
-  });
+    callback(items)
+    cityPicker.dispose()
+  })
 }
 
 export {
