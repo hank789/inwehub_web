fbad05f9a052b4b71d624f2c78c61ae020f77ef0
edwin <edwincoder@163.com>
2017-11-01 17:55:06 +0800

    fix share

--- asset/views/Contact.vue
+++ asset/views/Contact.vue
@@ -28,19 +28,19 @@
 
         </div>
       </Contact>
-
-      <Share
-        ref="ShareBtn"
-        :hideShareBtn="true"
-        :title="'邀您回答|' + title"
-        :link="shareUrl"
-        :content="'诚挚的邀请您前往参与回答和互动，已有' + answernum + '个回答、' + followednum + '个关注'"
-        :imageUrl="shareImg"
-        :thumbUrl="shareImg"
-        @success="shareSuccess"
-        @fail="shareFail"
-      ></Share>
     </div>
+
+    <Share
+      ref="ShareBtn"
+      :hideShareBtn="true"
+      :title="'邀您回答|' + title"
+      :link="shareUrl"
+      :content="'诚挚的邀请您前往参与回答和互动，已有' + answernum + '个回答、' + followednum + '个关注'"
+      :imageUrl="shareImg"
+      :thumbUrl="shareImg"
+      @success="shareSuccess"
+      @fail="shareFail"
+    ></Share>
   </div>
 </template>
 
