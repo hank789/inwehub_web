b9737d3f5094459dca0486d20fd2f948fb2c04cf
丁冉 <m17621626160@163.com>
2017-11-08 19:32:17 +0800

    两倍图

--- asset/components/invitation/image.vue
+++ asset/components/invitation/image.vue
@@ -1,34 +1,38 @@
 <template>
   <div>
-    <div id="shareContentWrapper" style="display:block; background: #EBECED;">
-      <div style="width:750px;" id="widthTotal">
-      <div id="images_t"
-        style="width:100%; height:440px;  background:-webkit-gradient(linear, 0 0, 0 bottom, from(#44474B), to(rgba(113, 117, 120, 1))); position: relative">
-        <img src="../../statics/images/logo_blue@3x.png" style="height: 24px; margin-top: 20px; margin-left: 17px"  />
-        <div style="width: 100%; height: 95px;  position: relative;">
-          <img src="../../statics/images/balance2.png"
-               style="width:69px; height: 69px; border-radius: 50%; position: absolute; left: 0; right:0; top: 0; bottom: 0; margin: auto; border: 2px solid #DBDCDB;">
-        </div>
-        <div
-          style="width:92%; margin-left: 4%; background:#fcc816; border-radius:14px; position: relative; padding: 10px 0;">
-          <span style="display:block; font-size:18px; color:#444444; font-weight: 600; text-align: center">章顾问向你发送了特权～
+    <div id="scaleWrapper">
+      <div id="shareContentWrapper" style="display:block; width:750px; background: #EBECED;">
+        <div style="width:750px;">
+          <div id="images_t"
+               style="width:100%; height:875px;  background:-webkit-gradient(linear, 0 0, 0 bottom, from(#44474B), to(rgba(113, 117, 120, 1))); position: relative">
+            <img src="../../statics/images/logo_blue@3x.png" style="height: 24px; margin-top: 20px; margin-left: 17px"/>
+            <div style="width: 100%; height:183px;  position: relative;">
+              <img src="../../statics/images/balance2.png"
+                   style="width:137.5px; height: width:137.5px; border-radius: 50%; position: absolute; left: 0; right:0; top: 0; bottom: 0; margin: auto; border: 2px solid #DBDCDB;">
+            </div>
+            <div
+              style="width:92%; height:144px; margin-left: 4%; background:#fcc816; border-radius:14px; position: relative; padding-top:40px">
+          <span style="display:block; font-size:36px; color:#444444; font-weight: 600; text-align: center; ">章顾问向你发送了特权～
           </span>
-          <span style="display:block; font-size:14px; color:#444444; text-align: center">邀请好友注册立享5%现金分红</span>
-          <i style="content: ''; display: block; width: 0; height: 0; border-left: 10px solid transparent; border-right: 10px solid transparent; border-bottom: 10px solid #fcc816; position: absolute; -webkit-transform: rotate(0deg); transform: rotate(0deg); left: 0px; right: 0; top: -10px; margin: auto;"></i>
-        </div>
-        <div style="width: 100%; height:117px;  position: relative;">
-           <div style="width:220px; height: 84px;  position: absolute; left: 0; right: 0; top: 0; bottom: 0; margin: auto">
-             <img src="../../statics/images/xiaoha-erweima.jpeg" style="width: 84px; height: 84px; border-radius: 10px; float: left;">
-             <div style="float:right; margin-top: 21px;">
-               <span style="display: block; font-size:15px; color: #ffffff;">扫描前往领取特权</span>
-               <span style="display: block; font-size:12px; color: #ffffff;">（首次登陆1元提问）</span>
-             </div>
-           </div>
-        </div>
+              <span style="display:block; font-size:28px; color:#444444; text-align: center;  margin-top:25px">邀请好友注册立享5%现金分红</span>
+              <i
+                style="content: ''; display: block; width: 0; height: 0; border-left: 10px solid transparent; border-right: 10px solid transparent; border-bottom: 10px solid #fcc816; position: absolute; -webkit-transform: rotate(0deg); transform: rotate(0deg); left: 0px; right: 0; top: -10px; margin: auto;"></i>
+            </div>
+            <div style="width: 100%; height:234px;  position: relative;">
+              <div
+                style="width:440px; height: 168px;  position: absolute; left: 0; right: 0; top: 0; bottom: 0; margin: auto">
+                <qr-code :text="'sfdfsfsfsfs'" :size="168" error-level="M" style="border-radius: 10px; border:9px solid #FFFFFF; float: left;"></qr-code>
+                <div style="float:right; margin-top:50px;">
+                  <span style="display: block; font-size:30px; color: #ffffff;">扫描前往领取特权</span>
+                  <span style="display: block; font-size:24px; color: #ffffff; margin-top: 20px">（首次登陆1元提问）</span>
+                </div>
+              </div>
+            </div>
 
+          </div>
+          <img src="../../statics/images/invitation_img@3x.png" style="width: 100%; position: relative; top: -260px; ">
+        </div>
       </div>
-      <img src="../../statics/images/invitation_img@3x.png" style="width: 100%; position: relative; top: -124px;" >
-    </div>
     </div>
   </div>
 </template>
@@ -36,10 +40,10 @@
 
 </script>
 <style scoped>
-#shareContentWrapper #widthTotal{
-  transform-origin:left top;
-  -webkit-transform: scale(.5);
-  transform: scale(.5);
-}
+  #scaleWrapper {
+    transform-origin: left top;
+    -webkit-transform: scale(.5);
+    transform: scale(.5);
+  }
 
 </style>
