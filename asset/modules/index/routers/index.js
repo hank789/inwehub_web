feab4d04009ea872ce2a87c65aea373637a3c1e1
edwin <edwincoder@163.com>
2017-11-27 15:27:53 +0800

    修改注释

--- asset/modules/index/routers/index.js
+++ asset/modules/index/routers/index.js
@@ -83,7 +83,7 @@ router.pushPlus = function (url, id = '', autoShow = true, aniShow = 'pop-in', p
       id = window.plus.runtime.appid
     } else if (id === nextUrl) {
       urlSplit = url.split('/')
-      // 判断是否列表页面
+      // 判断是否是详情页面, 详情页面id为list-detail-page
       if (urlSplit[urlSplit.length - 1] > 0) {
         id = 'list-detail-page'
       }
