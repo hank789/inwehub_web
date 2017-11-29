066059bcaa18d2133b729fd90bd3dbaaeea2035b
丁冉 <m17621626160@163.com>
2017-11-29 17:39:54 +0800

    add

--- asset/views/Discover/Add.vue
+++ asset/views/Discover/Add.vue
@@ -28,14 +28,10 @@
       </div>
 
       <div class="container-bottom-menus">
-<<<<<<< HEAD
-        <svg class="icon menu" aria-hidden="true" @tap.stop.prevent="$router.pushPlus('/my/advantage', 'list-detail-page')">
-=======
-        <svg class="icon menu" aria-hidden="true" @tap.stop.prevent="selectChannel()">
+        <svg class="icon menu" aria-hidden="true">
           <use xlink:href="#icon-icon-test1"></use>
         </svg>
-        <svg class="icon menu" aria-hidden="true" @tap.stop.prevent="selectChannel()">
->>>>>>> a3a80e5a67e11d57a4d4f237550add4009d5f787
+        <svg class="icon menu" aria-hidden="true" @tap.stop.prevent="$router.pushPlus('/my/advantage', 'list-detail-page')">
           <use xlink:href="#icon-icon-test"></use>
         </svg>
         <svg class="icon menu" aria-hidden="true" @tap.stop.prevent="jumpToLinkMode()">
