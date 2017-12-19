d3a45866683e25c9fdc2e73fc8b593e0e1b59f4f
丁冉 <m17621626160@163.com>
2017-12-19 11:07:44 +0800

    优化

--- asset/components/question-detail/Question.vue
+++ asset/components/question-detail/Question.vue
@@ -18,7 +18,7 @@
 
     </div>
     <!--添加图片-->
-    <Images class="container-images-discover img-style margin-10-0-0" :images="ask.data.img" :group="ask.id" v-if="ask.data.img.length > 0">
+    <Images class="container-images-discover img-style margin-10-0-0" :images="ask.data.img" :group="ask.id" v-if="ask.data ? ask.data.img.length > 0 : ''">
     </Images>
 
 
