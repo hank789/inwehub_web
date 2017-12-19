ea655d52e5cbf084d039996033798dc958b0b007
edwin <edwincoder@163.com>
2017-12-19 16:41:23 +0800

    del android 软键盘自动弹出

--- asset/utils/plus.js
+++ asset/utils/plus.js
@@ -211,11 +211,11 @@ function closeFullscreen () {
 function softInput () {
   window.mui.plusReady(function () {
     if (window.mui.os.android) {
-      var main = window.plus.android.runtimeMainActivity()
-      var Context = window.plus.android.importClass('android.content.Context')
-      var InputMethodManager = window.plus.android.importClass('android.view.inputmethod.InputMethodManager')
-      var imm = main.getSystemService(Context.INPUT_METHOD_SERVICE)
-      imm.toggleSoftInput(0, InputMethodManager.SHOW_FORCED)
+      // var main = window.plus.android.runtimeMainActivity()
+      // var Context = window.plus.android.importClass('android.content.Context')
+      // var InputMethodManager = window.plus.android.importClass('android.view.inputmethod.InputMethodManager')
+      // var imm = main.getSystemService(Context.INPUT_METHOD_SERVICE)
+      // imm.toggleSoftInput(0, InputMethodManager.SHOW_FORCED)
     } else {
       var nativeWebview = window.plus.webview.currentWebview().nativeInstanceObject()
       nativeWebview.plusCallMethod({
