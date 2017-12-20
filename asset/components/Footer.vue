8394d72672c420178728212a1bf67dea1142b062
hank <hank.huiwang@gmail.com>
2017-12-20 17:49:47 +0800

    优化微信通知

--- asset/components/Footer.vue
+++ asset/components/Footer.vue
@@ -85,6 +85,7 @@
         isMy: false,
         showBottom: true,
         isDiscover: false,
+        isSocketListened: false,
         taskCount: 0,
         message_total_count: 0,
         shareoption: {
@@ -99,6 +100,7 @@
     },
     props: {},
     mounted () {
+      this.listen()
       // this.$refs.short.show();
       window.addEventListener('refreshData', (e) => {
         // 执行刷新
@@ -129,8 +131,9 @@
       },
       listen () {
         var currentUser = localEvent.getLocalItem('UserLoginInfo')
-        if (currentUser.id && window.Echo) {
+        if (currentUser.id && window.Echo && this.isSocketListened === false) {
           console.log('listen notification')
+          this.isSocketListened = true
           window.Echo.options.auth.headers['Authorization'] = 'Bearer ' + currentUser.token
           // 监听通知事件
           window.Echo.channel('notification.user.' + currentUser.id)
@@ -221,7 +224,6 @@
       }
     },
     created () {
-      this.listen()
       var tmpArr = this.$route.path.split('/')
       var curPath = tmpArr[1] === '' ? 'home' : tmpArr[1]
       this.changeNav(curPath, this.$route.path)
