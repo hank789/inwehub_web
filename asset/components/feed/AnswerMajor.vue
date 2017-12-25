c89292da0fa9f04e2fdb85d577958f53407c5c09
丁冉 <m17621626160@163.com>
2017-12-25 11:48:03 +0800

    major

--- asset/components/feed/AnswerMajor.vue
+++ asset/components/feed/AnswerMajor.vue
@@ -14,28 +14,40 @@
     </div>
     <div class="text-16-444 mui-ellipsis-3">{{data.feed.title}}</div>
 
-    <div class="options text-right">
-      <div class="component-followed-question" v-if="data.feed.average_rate">{{data.feed.average_rate}}好评</div>
-      <div class="component-iconNumber iconPenglunWrapper">
-        <svg class="icon" aria-hidden="true">
-          <use xlink:href="#icon-pinglun1"></use>
-        </svg><span>{{data.feed.comment_number}}</span>
-      </div>
-      <div class="component-iconNumber">
-        <svg class="icon" aria-hidden="true">
-          <use xlink:href="#icon-dianzan1"></use>
-        </svg><span>{{data.feed.support_number}}</span>
-      </div>
-    </div>
-
     <div class="buttonWrapper">
       <button class="mui-btn mui-btn-block mui-btn-primary" v-if="!data.feed.is_pay_for_view">1元看答案／看评论</button>
       <button class="mui-btn mui-btn-block mui-btn-primary" v-else>查看回答</button>
     </div>
 
-    <div class="component-dianzanList" v-if="data.feed.support_number"><svg class="icon" aria-hidden="true">
-      <use xlink:href="#icon-dianzan1"></use>
-    </svg><span v-for="(item, index) in data.feed.supporter_list" @tap.stop.prevent="toResume(item.uuid)">{{item.name}}</span><span v-if="data.feed.support_number > data.feed.supporter_list.length">等{{data.feed.support_number}}人</span>
+
+    <div class="question_info">
+      <p>
+        <span>回答者:</span> {{data.user.name}}
+        <i v-if="data.feed.average_rate"></i>
+        <span v-if="data.feed.average_rate">{{data.feed.average_rate}}好评</span>
+      </p>
+      <p>
+        <span class="support_number">
+          <svg class="icon" aria-hidden="true" >
+          <use xlink:href="#icon-dianzan1"></use>
+          </svg>
+          <i v-if="data.feed.support_number">{{data.feed.support_number}}</i>
+        </span>
+        <span class="comment_number" >
+          <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-pinglun1"></use>
+          </svg>
+          <i v-if="data.feed.comment_number">{{data.feed.comment_number}}</i>
+        </span>
+      </p>
+    </div>
+
+    <div class="component-dianzanList" v-if="data.feed.support_number">
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-dianzan1"></use>
+      </svg>
+      <span v-for="(item, index) in data.feed.supporter_list" @tap.stop.prevent="toResume(item.uuid)">{{item.name}}</span>
+      <span v-if="data.feed.support_number > data.feed.supporter_list.length">等{{data.feed.support_number}}人</span>
     </div>
 
   </div>
@@ -113,5 +125,58 @@
 
   .buttonWrapper{
     margin-top:10px;
+  }
+  .component-dianzanList{
+    height: 43px;
+    background: #f3f4f6;
+    border-radius: 4px;
+    line-height: 43px;
+    padding-left: 15px;
+    margin-top: 13px;
+  }
+
+  /*回答者*/
+  .question_info{
+    height:19px;
+    /*background: #cccccc;*/
+    margin-top: 12px;
+    line-height:19px;
+  }
+  .question_info p:nth-of-type(1){
+    float: left;
+    font-size:13px;
+    color: #747474;
+  }
+  .question_info p:nth-of-type(1) i{
+    display: inline-block;
+    width:1px;
+    height:12px;
+    background: #dbdbdb;
+    margin:0 9px -1px 9px;
+  }
+  .question_info p:nth-of-type(1) span:nth-of-type(1){
+    color: #b4b4b6;
+  }
+  .question_info p:nth-of-type(2){
+    float: right;
+    font-size: 13px;
+    color: #808080;
+  }
+  .question_info p:nth-of-type(2) span{
+    float: right;
+  }
+  .question_info p:nth-of-type(2) span:nth-of-type(1){
+    margin-left: 22px;
+  }
+  .question_info p:nth-of-type(2) span:nth-of-type(1) svg{
+    font-size: 18px;
+    color: #808080;
+  }
+  .question_info p:nth-of-type(2) span:nth-of-type(2) svg{
+    font-size: 18px;
+    margin-bottom: -2px;
+  }
+  .question_info p:nth-of-type(2) i{
+    font-style: normal;
   }
 </style>
--- asset/components/feed/UpvotePayQuestion.vue
+++ asset/components/feed/UpvotePayQuestion.vue
@@ -12,33 +12,42 @@
         <div class="mui-media-body">{{data.title}}</div>
       </div>
     </div>
-    <div class="container-answer">
-      <div class="color-444 font-16 mui-ellipsis-2">{{data.feed.question_title}}</div>
-      <div class="color-808080 font-13 margin-5-0-0">回答者：{{data.feed.answer_user_name}}</div>
-    </div>
-    <!--点赞 关注问题-->
-    <div class="options text-right">
-      <div class="component-followed-question" v-if="data.feed.average_rate">{{data.feed.average_rate}}好评</div>
-      <div class="component-iconNumber iconPenglunWrapper">
-        <svg class="icon" aria-hidden="true">
-          <use xlink:href="#icon-pinglun1"></use>
-        </svg><span>{{data.feed.comment_number}}</span>
-      </div>
-      <div class="component-iconNumber">
-        <svg class="icon" aria-hidden="true">
-          <use xlink:href="#icon-dianzan1"></use>
-        </svg><span>{{data.feed.support_number}}</span>
-      </div>
-    </div>
+    <div class="text-16-444 mui-ellipsis-3">{{data.feed.question_title}}</div>
 
     <div class="buttonWrapper">
       <button class="mui-btn mui-btn-block mui-btn-primary" v-if="!data.feed.is_pay_for_view">1元看答案／看评论</button>
       <button class="mui-btn mui-btn-block mui-btn-primary" v-else>查看回答</button>
     </div>
 
-    <div class="component-dianzanList" v-if="data.feed.support_number"><svg class="icon" aria-hidden="true">
-      <use xlink:href="#icon-dianzan1"></use>
-    </svg><span v-for="(item, index) in data.feed.supporter_list" @tap.stop.prevent="toResume(item.uuid)">{{item.name}}</span><span v-if="data.feed.support_number > data.feed.supporter_list.length">等{{data.feed.support_number}}人</span>
+    <!--点赞 关注问题-->
+    <div class="question_info">
+      <p>
+        <span>回答者:</span> {{data.feed.answer_user_name}}
+        <i v-if="data.feed.average_rate"></i>
+        <span v-if="data.feed.average_rate">{{data.feed.average_rate}}好评</span>
+      </p>
+      <p>
+        <span class="support_number">
+          <svg class="icon" aria-hidden="true" >
+          <use xlink:href="#icon-dianzan1"></use>
+          </svg>
+          <i v-if="data.feed.support_number">{{data.feed.support_number}}</i>
+        </span>
+        <span class="comment_number" >
+          <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-pinglun1"></use>
+          </svg>
+          <i v-if="data.feed.comment_number">{{data.feed.comment_number}}</i>
+        </span>
+      </p>
+    </div>
+
+    <div class="component-dianzanList" v-if="data.feed.support_number">
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-dianzan1"></use>
+      </svg>
+      <span v-for="(item, index) in data.feed.supporter_list" @tap.stop.prevent="toResume(item.uuid)">{{item.name}}</span>
+      <span v-if="data.feed.support_number > data.feed.supporter_list.length">等{{data.feed.support_number}}人</span>
     </div>
 
   </div>
@@ -79,6 +88,59 @@
 <style scoped>
 
   .buttonWrapper{
-    margin-top:10px;
+    margin-top:15px;
+  }
+  .component-dianzanList{
+    height: 43px;
+    background: #f3f4f6;
+    border-radius: 4px;
+    line-height: 43px;
+    padding-left: 15px;
+    margin-top: 13px;
+  }
+
+  /*回答者*/
+  .question_info{
+    height:19px;
+    /*background: #cccccc;*/
+    margin-top: 12px;
+    line-height:19px;
+  }
+  .question_info p:nth-of-type(1){
+    float: left;
+    font-size:13px;
+    color: #747474;
+  }
+  .question_info p:nth-of-type(1) i{
+    display: inline-block;
+    width:1px;
+    height:12px;
+    background: #dbdbdb;
+    margin:0 9px -1px 9px;
+  }
+  .question_info p:nth-of-type(1) span:nth-of-type(1){
+    color: #b4b4b6;
+  }
+  .question_info p:nth-of-type(2){
+    float: right;
+    font-size: 13px;
+    color: #808080;
+  }
+  .question_info p:nth-of-type(2) span{
+    float: right;
+  }
+  .question_info p:nth-of-type(2) span:nth-of-type(1){
+    margin-left: 22px;
+  }
+  .question_info p:nth-of-type(2) span:nth-of-type(1) svg{
+    font-size: 18px;
+    color: #808080;
+  }
+  .question_info p:nth-of-type(2) span:nth-of-type(2) svg{
+    font-size: 18px;
+    margin-bottom: -2px;
+  }
+  .question_info p:nth-of-type(2) i{
+    font-style: normal;
   }
 </style>
