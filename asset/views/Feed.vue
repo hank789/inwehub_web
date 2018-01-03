c918954b26960f516da38e1927e5430a094b4861
hank <hank.huiwang@gmail.com>
2018-01-03 18:11:26 +0800

    debug

--- asset/views/Feed.vue
+++ asset/views/Feed.vue
@@ -180,7 +180,11 @@
       // 新手任务
       userAbility.newbieTask(this)
       autoTextArea()
-      window.mui.alert('小程序目前不支持微信支付，请<a href="https://adsolj.mlinks.cc/A0k8">点击下载App</a>访问！')
+      window.mui.confirm('小程序目前不支持微信支付，请点击下载App访问！', '提示', ['下次再说', '前往下载'], (e)=> {
+        if (e.index === 1) {
+          window.location.href = 'https://adsolj.mlinks.cc/' + process.env.DEEP_LINK_KEY
+        }
+      })
     },
     computed: {},
     methods: {
