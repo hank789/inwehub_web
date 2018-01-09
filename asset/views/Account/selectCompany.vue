95f77fa3a745d31b69460536ee8e40f9937149ea
丁冉 <m17621626160@163.com>
2018-01-09 16:54:41 +0800

    企业

--- asset/views/Account/selectCompany.vue
+++ asset/views/Account/selectCompany.vue
@@ -56,6 +56,7 @@
   import { getGeoPosition } from '../../utils/allPlatform'
   import RefreshList from '../../components/refresh/List.vue'
   import localEvent from '../../stores/localStorage'
+  import { checkPermissionLocation, toSettingSystemLocation } from '../../utils/plus'
   const currentUser = localEvent.getLocalItem('UserInfo')
 
   export default {
@@ -74,21 +75,36 @@
         value: '',
         data: '',
         distance: '',
-        dataList: null
+        dataList: null,
+        isLocation: false
       }
     },
     components: {
       RefreshList
     },
     created () {
-      this.searchRule = this.$route.query.from === 'infobasic' ? 2 : 1
-      getGeoPosition((position) => {
-        this.dataList = {
-          longitude: position.longt,
-          latitude: position.lat
-        }
-        this.longt = position.longt
-        this.lat = position.lat
+      checkPermissionLocation(() => {
+        this.isLocation = true
+        this.searchRule = this.$route.query.from === 'infobasic' ? 2 : 1
+        getGeoPosition((position) => {
+          this.dataList = {
+            longitude: position.longt,
+            latitude: position.lat
+          }
+          this.longt = position.longt
+          this.lat = position.lat
+        })
+      }, () => {
+        // 获取权限失败的回调
+        var btnArray = ['取消', '去设置']
+        window.mui.confirm('请在设置中打开定位服务，以启用地址定位或发现附近的企业和个人。', '无法启用定位模式', btnArray, (e) => {
+          if (e.index === 1) {
+            toSettingSystemLocation()
+          } else {
+            window.mui.back()
+          }
+        })
+        //
       })
     },
     methods: {
@@ -167,7 +183,7 @@
     },
     watch: {
       searchText: function (newValue) {
-        if (newValue) {
+        if (newValue && this.isLocation) {
           this.value = newValue
           this.dataList = {
             name: newValue,
