e30490f75f6dc16aec58c52182bffeaa1ff7fdcb
hank <hank.huiwang@gmail.com>
2018-01-16 11:05:00 +0800

    顾问小哈不展示邀请返现

--- asset/views/Account/my.vue
+++ asset/views/Account/my.vue
@@ -26,7 +26,7 @@
           </div>
 
         </div>
-        <div class="my-news" @tap.stop.prevent="$router.pushPlus('/invitation/index')">
+        <div v-if="show_my_wallet" class="my-news" @tap.stop.prevent="$router.pushPlus('/invitation/index')">
           <i>Hot</i>
           <span>邀请我的好友</span>
           <p><a>5%</a>返现等你拿</p>
