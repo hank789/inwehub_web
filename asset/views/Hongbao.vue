b3ba1fde736262d292624b8bbe7756b89c68a0a7
edwin <edwincoder@163.com>
2018-01-31 19:57:40 +0800

    map demos arr

--- asset/views/Hongbao.vue
+++ asset/views/Hongbao.vue
@@ -14,30 +14,39 @@
     mounted () {
       // 百度地图API功能
       var map = new window.BMap.Map('allmap')
-      var point = new window.BMap.Point(116.417854, 39.921988)
+      var point = new window.BMap.Point(121.525655, 31.08229)
       map.centerAndZoom(point, 15)
-      var opts = {
-        position: point,    // 指定文本标注所在的地理位置
-        offset: new window.BMap.Size(-15, -10)    // 设置文本偏移量
+
+      // 标注
+      var arr = [
+        {x: 121.525655, y: 31.08229, name: '张三'},
+        {x: 121.525655, y: 31.09229, name: '丁四'}
+      ]
+      for (var j = 0; j < arr.length; j++) {
+        var labelPoint = new window.BMap.Point(arr[j].x, arr[j].y)
+        var opts = {
+          position: labelPoint,    // 指定文本标注所在的地理位置
+          offset: new window.BMap.Size(-15, -10)    // 设置文本偏移量
+        }
+        var label = new window.BMap.Label(arr[j].name, opts)  // 创建文本标注对象
+        label.setStyle({
+          border: 'none',
+          background: 'none',
+          color: '#ffffff',
+          fontSize: '15px',
+          height: '20px',
+          lineHeight: '20px',
+          fontFamily: '微软雅黑'
+        })
+        map.addOverlay(label)
+
+        var circle = new window.BMap.Circle(
+          labelPoint,
+          200,
+          {strokeColor: '#fff', fillColor: '#03aef9', strokeWeight: 2, strokeOpacity: 0.5}) // 创建圆
+
+        map.addOverlay(circle)
       }
-      var label = new window.BMap.Label('张三', opts)  // 创建文本标注对象
-      label.setStyle({
-        border: 'none',
-        background: 'none',
-        color: '#ffffff',
-        fontSize: '15px',
-        height: '20px',
-        lineHeight: '20px',
-        fontFamily: '微软雅黑'
-      })
-
-      var circle = new window.BMap.Circle(
-        new window.BMap.Point(116.417854, 39.921988),
-        200,
-        {strokeColor: '#fff', fillColor: '#03aef9', strokeWeight: 2, strokeOpacity: 0.5}) // 创建圆
-
-      map.addOverlay(circle)
-      map.addOverlay(label)
     }
   }
 </script>
