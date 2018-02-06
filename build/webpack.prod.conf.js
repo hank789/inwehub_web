561652c64996d86c78e972b190397ec1f13b7783
edwin <edwincoder@163.com>
2018-02-06 18:39:43 +0800

    进度

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -104,7 +104,7 @@ if (config.build.bundleAnalyzerReport) {
 
 Object.keys(entris).forEach(function(entry) {
   var template = 'public/' + entry + '.html'
-  if (process.env.NODE_ENV_TYPE === 'browser') {
+  if (process.env.NODE_ENV_TYPE === '"browser"') {
     template = 'public/' + entry + '_browser.html'
   }
   webpackConfig.plugins.push(new HtmlWebpackPlugin({
