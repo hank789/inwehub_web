66f21a413e07f9e2418e73239fe0e9db717e71f5
hank <hank.huiwang@gmail.com>
2018-03-05 16:35:42 +0800

    优化

--- asset/utils/webview.js
+++ asset/utils/webview.js
@@ -29,7 +29,7 @@ function getIdByUrl (url, id) {
 function openWebviewByUrl (id, url, autoShow = true, aniShow = 'slide-in-right', popGesture = 'hide', reload = false) {
   window.mui.plusReady(function () {
     console.log('calledMethod: openWebviewByUrl, url:' + url + ', id:' + id)
-    id = getIdByUrl(url, id)
+    // id = getIdByUrl(url, id)
     var preloadBackClose = true
     if (id === url || id === 'backAndClose') {
       // 非特殊页面返回时关闭webview
