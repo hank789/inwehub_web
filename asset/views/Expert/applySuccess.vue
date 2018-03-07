6f55503cc8133286df803d95660b4de3caa57197
edwin <edwincoder@163.com>
2018-03-07 17:43:31 +0800

    fixed #1308

--- asset/views/Expert/applySuccess.vue
+++ asset/views/Expert/applySuccess.vue
@@ -58,11 +58,6 @@
 
 <style scoped="scoped">
   .mui-content {
-    position: absolute;
-    width: 100%;
-    top: 0;
-    left: 0;
-    bottom: 0;
     background: #ececee;
     text-align: center;
   }
