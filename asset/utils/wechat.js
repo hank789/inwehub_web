324b1deaf0b9465d35370acdcbd86fd9d50e38d5
edwin <edwincoder@163.com>
2018-03-09 14:52:43 +0800

    wechat test

--- asset/utils/wechat.js
+++ asset/utils/wechat.js
@@ -34,6 +34,11 @@ function hideHeaderHandler (obj, type) {
 
 function rebootAuth (hash) {
   if (window.mui.os.wechat) {
+    var userAgent = window.navigator.userAgent
+    if (userAgent.match(/WindowsWechat/)) {
+      return false
+    }
+
     if (process.env.NODE_ENV === 'development') {
       router.push('/login')
       return false
