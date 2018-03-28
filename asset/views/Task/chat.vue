398de8d0ebb78d1ba864ff2f82dbbde5db1483e2
丁冉 <m17621626160@163.com>
2018-03-28 16:27:32 +0800

    chat

--- asset/views/Task/chat.vue
+++ asset/views/Task/chat.vue
@@ -32,14 +32,15 @@
                    <SingleImage :src="item.data.img" :isSmallImage="item.data.img.length < 100" :group="currentUser.user_id + ''"></SingleImage>
                 </span>
               </p>
+              <!--未关注的权限提示-->
+              <!--<div  class="hint" v-if="index === -1">-->
+                <!--<p>34344343434</p>-->
+                <!--<p>-->
+                  <!--由于对方没有关注你，你只能发送3条消息，需对方关注或回复后才能恢复正常聊天-->
+                <!--</p>-->
+              <!--</div>-->
             </li>
-            <!--未关注的权限提示-->
-            <div  class="hint" v-if="index === -1">
-              <p>34344343434</p>
-              <p>
-                由于对方没有关注你，你只能发送3条消息，需对方关注或回复后才能恢复正常聊天
-              </p>
-            </div>
+
             <!--自己  -->
             <li class="Customerservice" v-else-if="currentUser.user_id == item.user_id">
               <p>{{showTime(list[index-1], item)}}</p>
@@ -51,7 +52,13 @@
                   <SingleImage :src="item.data.img" :isSmallImage="item.data.img.length < 100" :group="currentUser.user_id + ''"></SingleImage>
                 </span>
               </p>
-
+              <!--未关注的权限提示-->
+              <!--<div  class="hint" >-->
+                <!--<p>34344343434</p>-->
+                <!--<p>-->
+                  <!--由于对方没有关注你，你只能发送3条消息，需对方关注或回复后才能恢复正常聊天-->
+                <!--</p>-->
+              <!--</div>-->
             </li>
           </template>
         </ul>
