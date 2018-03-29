5fc8dbdb664d4f4491a5116fb428264272c31dcb
丁冉 <m17621626160@163.com>
2018-03-29 17:26:01 +0800

    我的发布超链接识别

--- asset/views/Account/publish/publishQuestions.vue
+++ asset/views/Account/publish/publishQuestions.vue
@@ -28,7 +28,7 @@
         class="listWrapper">
         <ul class="answer">
           <li v-for="(ask, index) in list" @tap.stop.prevent="toDetail(ask)">
-            <p class="mui-ellipsis-2">{{ask.description}}</p>
+            <p class="mui-ellipsis-2" v-html="textToLink(ask.description)"></p>
             <p>
               <span  class="label" :class="'label_' + ask.status">{{ask.status_description}}</span>
               <span>{{ask.created_at.split(' ')[0].replace(/-/g, '/') }}</span>
@@ -49,6 +49,7 @@
 <script>
   import RefreshList from '../../../components/refresh/List.vue'
   import localEvent from '../../../stores/localStorage'
+  import { textToLinkHtml, secureHtml } from '../../../utils/dom'
   const currentUser = localEvent.getLocalItem('UserInfo')
 
   const PublishAnswers = {
@@ -68,6 +69,9 @@
       RefreshList
     },
     methods: {
+      textToLink (text) {
+        return secureHtml(textToLinkHtml(text))
+      },
       toDetail (item) {
         if (item.question_type === 2) {
           this.$router.pushPlus('/askCommunity/interaction/answers/' + item.id)
