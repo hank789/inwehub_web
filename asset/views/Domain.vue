fcd2d2c6ff043dd588903d9d9729a04bde69e099
edwin <edwincoder@163.com>
2018-08-03 15:01:37 +0800

    领域列表去除数字

--- asset/views/Domain.vue
+++ asset/views/Domain.vue
@@ -43,11 +43,10 @@
 
         <div class="container-list-article">
           <template  v-for="(item, index) in list">
-            <div class="line-river-big" v-if="index === 5"></div>
             <div class="component-item-article" @tap.stop.prevent="toDetail(item)">
               <div class="itemArticleLeft">
                 <div class="titleWrapper">
-                  <div class="title text-line-2 text-content"><span class="number" v-if="index < 5">{{index+1}}.</span>{{item.data.title}}</div>
+                  <div class="title text-line-2 text-content">{{item.data.title}}</div>
                 </div>
                 <div class="explain">
                   <label v-if="item.tips">{{item.tips}}</label><span v-if="item.type_description">{{item.type_description}}</span><timeago :since="timeago(item.created_at)" :auto-update="60">
@@ -56,7 +55,7 @@
               </div>
               <div class="itemArticleRight"><img class="lazyImg" v-lazy="item.data.img"></div>
             </div>
-            <div class="line-river-after line-river-after-short" v-if="index !== 4 && index !== list.length-1"></div>
+            <div class="line-river-after line-river-after-short" v-if="index !== list.length-1"></div>
           </template>
         </div>
       </RefreshList>
