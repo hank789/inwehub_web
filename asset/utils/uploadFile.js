b56c5b486d9320efc9a23b5954054b8ba495e6da
edwin <edwincoder@163.com>
2018-08-09 16:33:47 +0800

    发布分享页uploadFile修改

--- asset/utils/uploadFile.js
+++ asset/utils/uploadFile.js
@@ -6,62 +6,64 @@
  */
 function selectFileH5 (acceptType = '', callback) {
   let fileInput = document.body.querySelector('input.upload-file[type=file]')
-  if (fileInput == null) {
+  if (fileInput === null) {
     fileInput = document.createElement('input')
     fileInput.setAttribute('type', 'file')
 
-    if (acceptType === 'img') {
-      fileInput.setAttribute('accept', 'image/png, image/gif, image/jpeg, image/jpg, image/bmp, image/x-icon')
-    }
-
-    if (acceptType === 'pdf') {
-      fileInput.setAttribute('accept', 'application/pdf')
-    }
-
     fileInput.classList.add('upload-file')
-    fileInput.addEventListener('change', () => {
-      if (fileInput.files != null && fileInput.files[0] != null) {
-        var file = fileInput.files[0]
-
-        var size = file.size / 1000  // kb
-
-        var warningText = ''
-
-        if (acceptType === 'pdf') {
-          if (size > 20480) {
-            warningText = '单个文件不允许超过20M！'
-            window.mui.alert(warningText)
-            return false
-          }
-        }
-
-        if (acceptType === 'img') {
-          if (size > 5120) {
-            warningText = '单张不允许超过5M！'
-            window.mui.alert(warningText)
-            return false
-          }
-        }
-
-        if (acceptType === 'pdf') {
-          if (file.type !== 'application/pdf') {
-            warningText = '请选择pdf文件！'
-            window.mui.alert(warningText)
-            return false
-          }
-        }
-
-        let reader = new FileReader()
-        reader.onload = (e) => {
-          fileInput.value = ''
-          document.body.removeChild(fileInput)
-          callback(file, e.target.result)
-        }
-        reader.readAsDataURL(file)
-      }
-    })
     document.body.appendChild(fileInput)
   }
+
+  if (acceptType === 'img') {
+    fileInput.setAttribute('accept', 'image/png, image/gif, image/jpeg, image/jpg, image/bmp, image/x-icon')
+  }
+
+  if (acceptType === 'pdf') {
+    fileInput.setAttribute('accept', 'application/pdf')
+  }
+
+  fileInput.addEventListener('change', () => {
+    if (fileInput.files !== null && fileInput.files[0] !== null) {
+      var file = fileInput.files[0]
+
+      var size = file.size / 1000  // kb
+
+      var warningText = ''
+
+      if (acceptType === 'pdf') {
+        if (size > 20480) {
+          warningText = '单个文件不允许超过20M！'
+          window.mui.alert(warningText)
+          return false
+        }
+      }
+
+      if (acceptType === 'img') {
+        if (size > 5120) {
+          warningText = '单张不允许超过5M！'
+          window.mui.alert(warningText)
+          return false
+        }
+      }
+
+      if (acceptType === 'pdf') {
+        if (file.type !== 'application/pdf') {
+          warningText = '请选择pdf文件！'
+          window.mui.alert(warningText)
+          return false
+        }
+      }
+
+      let reader = new FileReader()
+      reader.onload = (e) => {
+        fileInput.value = ''
+        document.body.removeChild(fileInput)
+        callback(file, e.target.result)
+      }
+      reader.readAsDataURL(file)
+    }
+  })
+
   fileInput.click()
 }
 
