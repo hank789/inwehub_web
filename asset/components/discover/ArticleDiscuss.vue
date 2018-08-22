d941c9853310cebe11221b39f6ce9443022a2678
陈灵灵 <798092775@qq.com>
2018-08-22 12:24:15 +0800

    隐藏数量为0的

--- asset/components/discover/ArticleDiscuss.vue
+++ asset/components/discover/ArticleDiscuss.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="component-block-title">
-      <div class="left">评论<span v-if="!total == 0">({{total}})</span></div>
+      <div class="left">评论<span v-if="total">({{total}})</span></div>
       <!--<div class="right" @tap.stop.prevent="switchMode">-->
         <!--<svg class="icon" aria-hidden="true">-->
           <!--<use xlink:href="#icon-paixu"></use>-->
