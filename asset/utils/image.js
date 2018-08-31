dfb7296b6f3f1515e3282a1bcf1826b071ebac5c
edwin <edwincoder@163.com>
2018-08-31 15:32:08 +0800

    加入 html doctype

--- asset/utils/image.js
+++ asset/utils/image.js
@@ -18,6 +18,8 @@ function saveHtmlImgToGallery (html, path, successCallback, failCallback) {
     window.mui.toast('仅支持APP内使用')
     // return
   }
+  html = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge"><title></title><style></style></head><body>' + html + '</body></html>'
+
   postRequest('system/htmlToImage', {html: html})
     .then(response => {
       var code = response.data.code
