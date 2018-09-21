a3f62b9cd8cc2c3c02d66aaf43573d19ab1d5a42
陈灵灵 <798092775@qq.com>
2018-09-21 14:31:05 +0800

    圈子

--- asset/views/Group/Add.vue
+++ asset/views/Group/Add.vue
@@ -4,40 +4,30 @@
       <Back></Back>
       <h1 class="mui-title">创建圈子</h1>
     </header>
-
     <div class="mui-content">
-      <div class="foundGroupWrapper">
-        <div class="foundGroupImages">
-          <img v-if="images.length" :id="'image_0'" :src="images[0].base64" :data-preview-src="images[0].base64" :data-preview-group="1"/>
-          <div class="foundGroupIcon" @tap.stop.prevent="uploadImage()" v-if="images.length < maxImageCount">
-            <svg class="icon" aria-hidden="true">
-              <use xlink:href="#icon-xiangji1"></use>
-            </svg>
-          </div>
+      <div class="imagesContainer">
+        <div class="groupImageWrapper" v-if="images.length">
+          <svg class="icon" aria-hidden="true" @tap.stop.prevent="delImg()">
+            <use xlink:href="#icon-times1"></use>
+          </svg>
+          <img :id="'image_0'" :src="images[0].base64" :data-preview-src="images[0].base64" :data-preview-group="1"/>
         </div>
-        <div class="headPhotowrapper">
-          <div class="headImages">
-            <svg class="icon logoImg" aria-hidden="true" v-if="!images.length">
-              <use xlink:href="#icon-biaozhunlogoshangxiayise"></use>
-            </svg>
-            <img v-if="images.length" :id="'image_0'" :src="images[0].base64" :data-preview-src="images[0].base64" :data-preview-group="1"/>
-            <div class="headPhotograph" @tap.stop.prevent="uploadImage()" v-if="images.length < maxImageCount">
-              <svg class="icon" aria-hidden="true">
-                <use xlink:href="#icon-xiangji1"></use>
-              </svg>
-            </div>
-          </div>
+        <div class="container-image component-photograph" @tap.stop.prevent="uploadImage()"
+             v-if="images.length < maxImageCount">
+          <svg class="icon" aria-hidden="true">
+            <use xlink:href="#icon-xiangji1"></use>
+          </svg>
         </div>
       </div>
 
-      <div class="groupNAme">
-        <input type="text" placeholder="圈子名称" v-model.trim="name" maxlength="9">
+      <div class="address">
+        <p>圈子名称</p>
+        <input type="text" placeholder="圈子名称9个字以内" v-model.trim="name" maxlength="9"/>
+        <i class="bot"></i>
       </div>
-      <div class="line-river-big"></div>
-
-      <div class="groupDescribe">
-        <div class="groupDescribeText">圈子描述</div>
-        <div class="form-ask">
+      <div class="groups-content">
+        <p>圈子描述</p>
+        <div class="form form-ask">
           <div class="textarea-wrapper">
             <textarea id="description" v-model.trim="description" placeholder="圈子的具体描述"></textarea>
           </div>
@@ -49,10 +39,11 @@
           <span class="niming" @tap.stop.prevent="selectType(1)"><label class="nimingCheckbox"
                                                                         :class="{'active':type}"></label>公开</span>
         <span class="niming" @tap.stop.prevent="selectType(0)"><label class="nimingCheckbox"
-                                                                      :class="{'active':!type}"></label>秘密<i>（仅入圈成员可看，创建后不可改)</i></span>
+                                                                      :class="{'active':!type}"></label>秘密<i>（内容仅入圈成员可看）</i></span>
       </div>
 
-      <div class="goFoundGroup font-family-medium" @tap.stop.prevent="submit()">开始创建</div>
+
+      <button @tap.stop.prevent="submit()">开始创建</button>
     </div>
 
     <uploadImage ref="uploadImage"
@@ -62,18 +53,16 @@
     ></uploadImage>
   </div>
 </template>
-
 <script>
   import uploadImage from '../../components/uploadImage'
   import { postRequest } from '../../utils/request'
-
   export default {
     data () {
       return {
         images: [],
-        name: '',
         maxImageCount: 1,
         description: '',
+        name: '',
         descMaxLength: 1000,
         type: 1  // 1 public  0 私密
       }
@@ -85,6 +74,10 @@
     },
     components: {
       uploadImage
+    },
+    created () {
+    },
+    mounted () {
     },
     watch: {
       description: function (newDescription) {
@@ -132,220 +125,228 @@
           this.$router.replace('/group/detail/' + response.data.data.id)
         })
       },
+      selectType (type) {
+        this.type = type
+      },
+      uploadImage: function () {
+        this.$refs.uploadImage.uploadImage()
+      },
       uploadImageSuccess (images) {
         for (var i = 0; i < images.length; i++) {
           this.images.push(images[i])
         }
       },
-      uploadImage: function () {
-        this.$refs.uploadImage.uploadImage()
-      },
-      selectType (type) {
-        this.type = type
+      delImg () {
+        this.images = []
       }
     }
   }
 </script>
+<style scoped>
+  /*清掉自带样式*/
 
-<style lang="less" scoped>
+  div,
+  p,
+  span,
+  i,
+  img,
+  ul,
+  li,
+  a,
+  input,
+  button {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    font-style: normal;
+  }
+
+  .bot {
+    position: absolute;
+    right: 0rem;
+    bottom: 0;
+    left: 0rem;
+    height: 0.026rem;
+    -webkit-transform: scaleY(.5);
+    transform: scaleY(.5);
+    background-color: rgb(220, 220, 220);
+  }
+
   .mui-content {
-    background: #ffffff;
+    background: #fff;
   }
-  .foundGroupWrapper {
+  .groupImageWrapper{
     position: relative;
-    .foundGroupImages {
-      height: 149px;
-      position: relative;
-      border-bottom-left-radius: 20px;
-      border-bottom-right-radius: 20px;
-      background:linear-gradient(180deg,rgba(0,0,0,0.3) 0%,rgba(0,0,0,0.1) 100%);
-      img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-        border-bottom-left-radius: 20px;
-        border-bottom-right-radius: 20px;
-      }
-      .foundGroupIcon {
-        font-size: 30px;
-        color: #ffffff;
-        text-align: center;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%,-50%);
-        z-index: 100;
-        .icon {
-        }
-      }
-    }
-    .headPhotowrapper {
-      position: absolute;
-      bottom: -24px;
-      left: 17px;
-      z-index: 100;
-      .headImages {
-        width: 56px;
-        height: 56px;
-        text-align: center;
-        background: #E3E3E3;
-        border-radius: 4px;
-        border: 1px solid #ffffff;
-        img {
-          width: 100%;
-          height: 100%;
-          border-radius: 3px;
-          object-fit: cover;
-        }
-        .logoImg {
-          color: #8B8B8B;
-          font-size: 30px;
-          margin-top: 11px;
-        }
-        .headPhotograph {
-          width: 54px;
-          height: 54px;
-          position: absolute;
-          top: 1px;
-          left: 1px;
-          text-align: center;
-          border-radius: 4px;
-          background:rgba(0,0,0,0.3);
-          .icon {
-            font-size: 18px;
-            color: #ffffff;
-            margin-top: 19px;
-          }
-        }
-      }
-    }
+    margin-top: 0.133rem;
   }
-  .groupNAme {
-    margin-top: 40px;
-    /*background: #DCDCDC;*/
-    input[type='text'] {
-      margin-bottom: 0;
-      border: none;
-      &::-webkit-input-placeholder {
-        color: #C8C8C8;
-        font-size: 20px;
-        font-family: PingFangSC-Medium;
-      }
-      &:-ms-input-placeholder {
-        color: #C8C8C8;
-        font-size: 20px;
-        font-family: PingFangSC-Medium;
-      }
-      &::-ms-input-placeholder {
-        color: #C8C8C8;
-        font-size: 20px;
-        font-family: PingFangSC-Medium;
-      }
-      &::placeholder {
-        color: #C8C8C8;
-        font-size: 20px;
-        font-family: PingFangSC-Medium;
-      }
-    }
+  .groupImageWrapper svg{
+    position: absolute;
+    top:-0.133rem;
+    right:-0.133rem;
+  }
+  .groupImageWrapper img {
+    width: 1.626rem;
+    height: 1.626rem;
+    border-radius: 0.106rem;
   }
 
-  .groupDescribe {
+  .component-photograph {
+    width: 1.626rem !important;
+    height: 1.626rem !important;
+  }
+
+  .imagesContainer {
+    width: 100%;
+    overflow: hidden;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    margin-top: 0.266rem;
+  }
+
+  .address {
+    width: 92%;
+    height: 1.146rem;
+    margin-left: 4%;
     position: relative;
-    padding: 11px 16px;
-    .groupDescribeText {
-      color: #808080;
-      font-size: 14px;
-    }
-    .form-ask {
-      .textarea-wrapper {
-        height: 164px;
-        background: #ffffff;
-        margin-top: 13px;
-        border: 0.026rem solid #DCDCDC;
-        border-radius: 0.133rem;
-        textarea {
-          background: #ffffff;
-          font-size: 0.373rem;
-          color: #444444;
-          height: 131px;
-          border: none;
-        }
-      }
-    }
-    .counter {
-      font-size: 14px;
-      color: #C8C8C8;
-      line-height: 20px;
-      position: absolute;
-      bottom: 17px;
-      right: 28px;
-    }
+    line-height: 1.146rem;
+    margin-top: 0.746rem;
+  }
+
+  .address p {
+    float: left;
+    color: #808080;
+    font-size: 0.373rem;
+  }
+
+  .address input {
+    width: 80%;
+    height: 1.12rem;
+    float: left;
+    margin-bottom: 0;
+    padding-right: 0;
+    padding-left: 0.4rem;
+    border: none;
+    font-size: 0.373rem;
+    color: #444444;
+    text-align: right;
+  }
+
+  .groups-content {
+    width: 92%;
+    overflow: hidden;
+    margin-left: 4%;
+    margin-top: 0.266rem;
+    position: relative;
+  }
+
+  .groups-content p {
+    font-size: 0.373rem;
+    color: #808080;
+  }
+
+  .form-ask .button-wrapper {
+    margin-top: 0.4rem;
+    padding: 0 2.133rem
+  }
+
+  .textarea-wrapper {
+    height: 5.333rem;
+    background: #ffffff;
+    margin-top: 0.346rem;
+    border: 0.026rem solid #DCDCDC;
+    border-radius: 0.133rem;
+  }
+
+  .textarea-wrapper textarea {
+    background: #ffffff;
+    font-size: 0.373rem;
+    color: #444444;
+    height: 4.373rem;
+    border: none;
+  }
+
+  .counter {
+    font-size: 0.373rem;
+    color: #C8C8C8;
+    position: absolute;
+    bottom: 0.16rem;
+    right: 0.266rem;
   }
 
   .fixedContainer {
     position: relative;
     background: #ffff;
-    padding: 1px 12px;
-    /*margin-top: 0.133rem;*/
-    .niming {
-      color: #444444;
-      position: relative;
-      font-size: 0.373rem;
-      padding-left: 0.24rem;
-      margin-right: 0.6rem;
-      &:nth-of-type(2) {
-        margin-right: 0;
-      }
-      i {
-        color: #808080;
-        font-size: 0.373rem;
-        font-style: normal;
-      }
-      input {
-        position: absolute;
-        top: 0.106rem;
-        left: 0;
-        vertical-align: bottom;
-      }
-    }
-    .nimingCheckbox {
-      display: inline-block;
-      width: 0.426rem;
-      height: 0.426rem;
-      position: relative;
-      border: 0.026rem solid #c8c8c8;
-      border-radius: 50%;
-      top: 0.08rem;
-      right: 0.133rem;
-      &.active {
-        &:after {
-          content: ' ';
-          display: inline-block;
-          width: 0.106rem;
-          height: 0.106rem;
-          position: absolute;
-          border: 0.026rem solid #03aef9;
-          background-color: #03aef9;
-          border-radius: 50%;
-          left: 50%;
-          margin-left: -0.07rem;
-          top: 50%;
-          margin-top: -0.08rem;
-        }
-      }
-    }
+    padding: 0.133rem 0.3rem;
+    margin-top: 0.133rem;
   }
 
-  .goFoundGroup {
-    width: 343px;
-    height: 44px;
-    border:none;
-    color: #FFFFFF;
-    font-size: 16px;
-    text-align: center;
-    background: #03AEF9;
-    line-height: 44px;
-    margin: 80px auto 0;
+  .fixedContainer .counter {
+    float: right;
+    color: #c8c8c8;
+  }
+
+  .fixedContainer .niming {
+    color: #444444;
+    position: relative;
+    font-size: 0.373rem;
+    padding-left: 0.24rem;
+    margin-right: 0.8rem;
+  }
+
+  .fixedContainer .niming i {
+    color: #808080;
+    font-size: 0.373rem;
+  }
+
+  .fixedContainer .niming input {
+    position: absolute;
+    top: 0.106rem;
+    left: 0;
+    vertical-align: bottom;
+  }
+
+  .nimingCheckbox {
+    display: inline-block;
+    width: 0.426rem;
+    height: 0.426rem;
+    position: relative;
+    border: 0.026rem solid #c8c8c8;
+    border-radius: 50%;
+    top: 0.08rem;
+    right: 0.133rem;
+  }
+
+  .nimingCheckbox.active:after {
+    content: ' ';
+    display: inline-block;
+    width: 0.106rem;
+    height: 0.106rem;
+    position: absolute;
+    border: 0.026rem solid #03aef9;
+    background-color: #03aef9;
+    border-radius: 50%;
+    left: 50%;
+    margin-left: -0.08rem;
+    top: 50%;
+    margin-top: -0.08rem;
+  }
+
+  button {
+    width: 92%;
+    height: 1.173rem;
     border-radius: 1.333rem;
+    background: #03AEF9;
+    font-size: 0.426rem;
+    color: #FFFFFF;
+    line-height: 1.173rem;
+    margin-left: 4%;
+    margin-top: 0.533rem;
+    border:none;
+  }
+
+  button.grey {
+    background: rgba(220, 220, 220, 1);
+    color: #B4B4B6;
   }
 </style>
---
+++ asset/views/Group/NewAdd.vue
@@ -0,0 +1,351 @@
+<template>
+  <div>
+    <header class="mui-bar mui-bar-nav">
+      <Back></Back>
+      <h1 class="mui-title">创建圈子</h1>
+    </header>
+
+    <div class="mui-content">
+      <div class="foundGroupWrapper">
+        <div class="foundGroupImages">
+          <img v-if="images.length" :id="'image_0'" :src="images[0].base64" :data-preview-src="images[0].base64" :data-preview-group="1"/>
+          <div class="foundGroupIcon" @tap.stop.prevent="uploadImage()" v-if="images.length < maxImageCount">
+            <svg class="icon" aria-hidden="true">
+              <use xlink:href="#icon-xiangji1"></use>
+            </svg>
+          </div>
+        </div>
+        <div class="headPhotowrapper">
+          <div class="headImages">
+            <svg class="icon logoImg" aria-hidden="true" v-if="!images.length">
+              <use xlink:href="#icon-biaozhunlogoshangxiayise"></use>
+            </svg>
+            <img v-if="images.length" :id="'image_0'" :src="images[0].base64" :data-preview-src="images[0].base64" :data-preview-group="1"/>
+            <div class="headPhotograph" @tap.stop.prevent="uploadImage()" v-if="images.length < maxImageCount">
+              <svg class="icon" aria-hidden="true">
+                <use xlink:href="#icon-xiangji1"></use>
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="groupNAme">
+        <input type="text" placeholder="圈子名称" v-model.trim="name" maxlength="9">
+      </div>
+      <div class="line-river-big"></div>
+
+      <div class="groupDescribe">
+        <div class="groupDescribeText">圈子描述</div>
+        <div class="form-ask">
+          <div class="textarea-wrapper">
+            <textarea id="description" v-model.trim="description" placeholder="圈子的具体描述"></textarea>
+          </div>
+        </div>
+        <span class="counter"><span>{{descLength}}</span><span>/</span><span>{{descMaxLength}}</span></span>
+      </div>
+
+      <div class="fixedContainer">
+          <span class="niming" @tap.stop.prevent="selectType(1)"><label class="nimingCheckbox"
+                                                                        :class="{'active':type}"></label>公开</span>
+        <span class="niming" @tap.stop.prevent="selectType(0)"><label class="nimingCheckbox"
+                                                                      :class="{'active':!type}"></label>秘密<i>（仅入圈成员可看，创建后不可改)</i></span>
+      </div>
+
+      <div class="goFoundGroup font-family-medium" @tap.stop.prevent="submit()">开始创建</div>
+    </div>
+
+    <uploadImage ref="uploadImage"
+                 :isMultiple="true"
+                 @success="uploadImageSuccess"
+                 :ImageMaximum="maxImageCount - this.images.length"
+    ></uploadImage>
+  </div>
+</template>
+
+<script>
+  import uploadImage from '../../components/uploadImage'
+  import { postRequest } from '../../utils/request'
+
+  export default {
+    data () {
+      return {
+        images: [],
+        name: '',
+        maxImageCount: 1,
+        description: '',
+        descMaxLength: 1000,
+        type: 1  // 1 public  0 私密
+      }
+    },
+    computed: {
+      descLength () {
+        return this.description.length
+      }
+    },
+    components: {
+      uploadImage
+    },
+    watch: {
+      description: function (newDescription) {
+        if (newDescription.length > this.descMaxLength) {
+          this.description = this.description.slice(0, this.descMaxLength)
+        }
+      }
+    },
+    methods: {
+      submit () {
+        if (!this.images.length) {
+          window.mui.toast('请选择图片')
+          return
+        }
+
+        if (!this.name.length) {
+          window.mui.toast('请输入圈子名称')
+          return
+        }
+
+        if (this.name.length > 9) {
+          window.mui.toast('圈子名称9个字以内')
+          return
+        }
+
+        if (!this.description.length) {
+          window.mui.toast('请输入圈子描述')
+          return
+        }
+
+        var data = {
+          name: this.name,
+          description: this.description,
+          logo: this.images[0].base64,
+          public: this.type === 1 ? 1 : 0
+        }
+
+        postRequest(`group/store`, data).then(response => {
+          var code = response.data.code
+          if (code !== 1000) {
+            window.mui.toast(response.data.message)
+            return
+          }
+
+          this.$router.replace('/group/detail/' + response.data.data.id)
+        })
+      },
+      uploadImageSuccess (images) {
+        for (var i = 0; i < images.length; i++) {
+          this.images.push(images[i])
+        }
+      },
+      uploadImage: function () {
+        this.$refs.uploadImage.uploadImage()
+      },
+      selectType (type) {
+        this.type = type
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  .mui-content {
+    background: #ffffff;
+  }
+  .foundGroupWrapper {
+    position: relative;
+    .foundGroupImages {
+      height: 149px;
+      position: relative;
+      border-bottom-left-radius: 20px;
+      border-bottom-right-radius: 20px;
+      background:linear-gradient(180deg,rgba(0,0,0,0.3) 0%,rgba(0,0,0,0.1) 100%);
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        border-bottom-left-radius: 20px;
+        border-bottom-right-radius: 20px;
+      }
+      .foundGroupIcon {
+        font-size: 30px;
+        color: #ffffff;
+        text-align: center;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        z-index: 100;
+        .icon {
+        }
+      }
+    }
+    .headPhotowrapper {
+      position: absolute;
+      bottom: -24px;
+      left: 17px;
+      z-index: 100;
+      .headImages {
+        width: 56px;
+        height: 56px;
+        text-align: center;
+        background: #E3E3E3;
+        border-radius: 4px;
+        border: 1px solid #ffffff;
+        img {
+          width: 100%;
+          height: 100%;
+          border-radius: 3px;
+          object-fit: cover;
+        }
+        .logoImg {
+          color: #8B8B8B;
+          font-size: 30px;
+          margin-top: 11px;
+        }
+        .headPhotograph {
+          width: 54px;
+          height: 54px;
+          position: absolute;
+          top: 1px;
+          left: 1px;
+          text-align: center;
+          border-radius: 4px;
+          background:rgba(0,0,0,0.3);
+          .icon {
+            font-size: 18px;
+            color: #ffffff;
+            margin-top: 19px;
+          }
+        }
+      }
+    }
+  }
+  .groupNAme {
+    margin-top: 40px;
+    /*background: #DCDCDC;*/
+    input[type='text'] {
+      margin-bottom: 0;
+      border: none;
+      &::-webkit-input-placeholder {
+        color: #C8C8C8;
+        font-size: 20px;
+        font-family: PingFangSC-Medium;
+      }
+      &:-ms-input-placeholder {
+        color: #C8C8C8;
+        font-size: 20px;
+        font-family: PingFangSC-Medium;
+      }
+      &::-ms-input-placeholder {
+        color: #C8C8C8;
+        font-size: 20px;
+        font-family: PingFangSC-Medium;
+      }
+      &::placeholder {
+        color: #C8C8C8;
+        font-size: 20px;
+        font-family: PingFangSC-Medium;
+      }
+    }
+  }
+
+  .groupDescribe {
+    position: relative;
+    padding: 11px 16px;
+    .groupDescribeText {
+      color: #808080;
+      font-size: 14px;
+    }
+    .form-ask {
+      .textarea-wrapper {
+        height: 164px;
+        background: #ffffff;
+        margin-top: 13px;
+        border: 0.026rem solid #DCDCDC;
+        border-radius: 0.133rem;
+        textarea {
+          background: #ffffff;
+          font-size: 0.373rem;
+          color: #444444;
+          height: 131px;
+          border: none;
+        }
+      }
+    }
+    .counter {
+      font-size: 14px;
+      color: #C8C8C8;
+      line-height: 20px;
+      position: absolute;
+      bottom: 17px;
+      right: 28px;
+    }
+  }
+
+  .fixedContainer {
+    position: relative;
+    background: #ffff;
+    padding: 1px 12px;
+    /*margin-top: 0.133rem;*/
+    .niming {
+      color: #444444;
+      position: relative;
+      font-size: 0.373rem;
+      padding-left: 0.24rem;
+      margin-right: 0.6rem;
+      &:nth-of-type(2) {
+        margin-right: 0;
+      }
+      i {
+        color: #808080;
+        font-size: 0.373rem;
+        font-style: normal;
+      }
+      input {
+        position: absolute;
+        top: 0.106rem;
+        left: 0;
+        vertical-align: bottom;
+      }
+    }
+    .nimingCheckbox {
+      display: inline-block;
+      width: 0.426rem;
+      height: 0.426rem;
+      position: relative;
+      border: 0.026rem solid #c8c8c8;
+      border-radius: 50%;
+      top: 0.08rem;
+      right: 0.133rem;
+      &.active {
+        &:after {
+          content: ' ';
+          display: inline-block;
+          width: 0.106rem;
+          height: 0.106rem;
+          position: absolute;
+          border: 0.026rem solid #03aef9;
+          background-color: #03aef9;
+          border-radius: 50%;
+          left: 50%;
+          margin-left: -0.07rem;
+          top: 50%;
+          margin-top: -0.08rem;
+        }
+      }
+    }
+  }
+
+  .goFoundGroup {
+    width: 343px;
+    height: 44px;
+    border:none;
+    color: #FFFFFF;
+    font-size: 16px;
+    text-align: center;
+    background: #03AEF9;
+    line-height: 44px;
+    margin: 80px auto 0;
+    border-radius: 1.333rem;
+  }
+</style>
