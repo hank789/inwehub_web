bb8b2f0d10b032bd5447d9d6b167e9d8046f0225
陈灵灵 <798092775@qq.com>
2018-10-08 14:43:53 +0800

    实名认证弹框

--- asset/utils/dialogList.js
+++ asset/utils/dialogList.js
@@ -385,7 +385,7 @@ function alertPhoneBindWarning (context, title, phone, avatar, isVip, name, butt
 
 // 去实名认证
 function alsrtRealNameAuthentication (callback) {
-  alertSkyTwo('<span class="font-family-medium realNameTitle">请完成实名认证</span> <span class="netWorkTitle">使用互联网服务需依法实名认证</span>', '<span class="authentication alertConfirm">去认证</span>', 'icon-chengweizhuanjia1', (num) => {
+  alertSkyTwo('<span class="font-family-medium authenticationRealNameTitle">请完成实名认证</span> <span class="authenticationNetWorkTitle">使用互联网服务需依法实名认证</span>', '<span class="goAuthentication alertConfirm">去认证</span>', 'icon-chengweizhuanjia1', (num) => {
     if (num.index === 0) {
       callback()
     }
