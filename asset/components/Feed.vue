bd691ed96808714cd3ecfe2f65767e3b6b8a114b
edwin <edwincoder@163.com>
2018-10-16 11:04:47 +0800

    add feed component

---
+++ asset/components/Feed.vue
@@ -0,0 +1,22 @@
+<template>
+
+</template>
+
+<script type="text/javascript">
+
+  export default {
+    data () {
+      return {}
+    },
+    components: {},
+    props: {
+      description: {
+        type: String,
+        default: '暂时还没有数据呀～'
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+</style>
