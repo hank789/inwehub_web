e77850145c23000e9fc9a621900515085c2e7f33
edwin <edwincoder@163.com>
2018-11-01 12:03:34 +0800

    add dropdown-menu skeleton

---
+++ asset/components/select/DropDownMenu.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+     
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {}
+    },
+    components: {},
+    props: {
+      tree: {
+        type: Object,
+        default: function () {
+          return {}
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped="scoped">
+
+</style>
