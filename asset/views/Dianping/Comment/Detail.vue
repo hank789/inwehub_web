9587fe0d5fd901b1bbf66b23c2a24ed42c6bb520
陈灵灵 <798092775@qq.com>
2018-11-05 21:08:22 +0800

    点评详情

--- asset/views/Dianping/Comment/Detail.vue
+++ asset/views/Dianping/Comment/Detail.vue
@@ -61,7 +61,7 @@
             </div>
           </div>
           <!-- 新增链接样式 -->
-          <div class="link">
+          <div class="link" v-if="detail.tags">
             <div class="linkBox" @tap.stop.prevent="goArticle(detail)">
               <span class="linkIimg" v-if="!detail.tags[0].logo">
                 <svg class="icon" aria-hidden="true" >
@@ -95,12 +95,79 @@
           @delCommentSuccess="delCommentSuccess"
           ref="discuss"
         ></ArticleDiscuss>
+        <div class="river"></div>
+
+        <div class="allDianPing font-family-medium">大家都在评</div>
+        <div class="productList">
+
+          <div class="comment-product" v-for="(item, index) in detail.related_tags" :key="index">
+            <div class="product-info"  @tap.stop.prevent="$router.pushPlus('/dianping/product/' + item.name)">
+              <div class="product-img">
+                <img class="lazyImg" v-lazy="item.logo" alt="">
+              </div>
+              <div class="product-detail">
+                <div class="productName font-family-medium text-line-1">{{ item.name }}</div>
+                <div class="productMark">
+                  <svg class="icon" aria-hidden="true">
+                    <use xlink:href="#icon-shoucangdilantongyi"></use>
+                  </svg><span>{{ item.review_average_rate }}分</span>
+                  <i></i><span>{{ item.review_count }}条评论</span>
+                </div>
+              </div>
+            </div>
+            <div class="line-river-after line-river-after-top" v-if="index !== 4 && index !== detail.related_tags.length-1"></div>
+          </div>
+        </div>
+        <div class="river"></div>
+        <div class="openAppReadBox">
+          <div class="openAppRead" @tap.stop.prevent="openApp()">
+            <span class="font-family-medium">打开APP</span>
+            <span>阅读更多推荐</span>
+          </div>
+          <div class="river openAppReadRiver"></div>
+          <div class="followCode">
+            <div class="CodeImg">
+              <img src="../../../statics/images/group_3@3x 2.png" alt="">
+            </div>
+            <div class="codeText">
+              <div>长按识别二维码，关注InweHub服务号</div>
+              <div>探索更多资讯/人脉/服务</div>
+            </div>
+          </div>
+          <div class="river openAppReadRiver"></div>
+        </div>
 
       </vue-pull-refresh>
     </div>
-
-
     <commentTextarea ref="ctextarea" @sendMessage="sendMessage"></commentTextarea>
+
+    <div class="container-footer" @tap.capture="onTap($event)">
+      <div class="footerLeft">
+        <div class="footerMenuOne" :class="detail.is_upvoted ? 'activeBlue':'activeRed'" v-if="detail.is_downvoted || detail.is_upvoted">{{detail.support_description}}</div>
+        <div class="footerMenuTwo" v-else>
+          <div class="noBullish containerBtn" @tap.stop.prevent="detailDownVote()">{{detail.downvote_tip}}</div>
+          <div class="bullish containerBtn" @tap.stop.prevent="upVote()">{{detail.support_tip}}</div>
+        </div>
+      </div>
+      <div class="footerRight">
+        <div class="collectionComment" @tap.stop.prevent="collection()">
+          <div>
+            <svg class="icon" aria-hidden="true" :class="{active: detail.is_bookmark}">
+              <use xlink:href="#icon-shoucangdilantongyi"></use>
+            </svg>
+          </div>
+          <span>收藏<i v-if="detail.bookmarks">{{detail.bookmarks}}</i></span>
+        </div>
+        <div class="collectionComment" @tap.stop.prevent="goComment()">
+          <div>
+            <svg class="icon" aria-hidden="true">
+              <use xlink:href="#icon-pinglun"></use>
+            </svg>
+          </div>
+          <span>评论<i v-if="detail.comments_number">{{detail.comments_number}}</i></span>
+        </div>
+      </div>
+    </div>
 
   </div>
 </template>
@@ -139,7 +206,8 @@
             toolbar: []
           },
           readOnly: true
-        }
+        },
+        tags: {}
       }
     },
     components: {
@@ -149,6 +217,9 @@
       'vue-pull-refresh': VuePullRefresh
     },
     methods: {
+      refreshPageData () {
+        this.getDetail()
+      },
       goComment () {
         this.$refs.discuss.rootComment()
         // scrollToElement(this, '#commentTitle', '.pull-down-container')
@@ -169,6 +240,12 @@
       comment (commentTargetName) {
         this.$refs.ctextarea.comment(commentTargetName)
       },
+      getDetail () {
+        this.id = this.$route.params.id
+        getCommentDetail(this, this.id, (detail) => {
+          this.detail = detail
+        })
+      },
       discussStoreParams () {
         return {'submission_id': this.detail.id}
       },
@@ -180,15 +257,6 @@
       },
       textToLink (text) {
         return transferTagToLink(textToLinkHtml(' ' + text))
-      },
-      getDetail () {
-        this.id = this.$route.params.id
-        getCommentDetail(this, this.id, (detail) => {
-          this.detail = detail
-        })
-      },
-      refreshPageData () {
-        this.getDetail()
       },
       change (editor) {
         var html = editor.html
@@ -226,7 +294,9 @@
         }, 100)
       }
     },
-    mounted () {},
+    mounted () {
+      console.log(this.detail.id + 'this.detail.slug')
+    },
     created () {
       this.getDetail()
     },
@@ -246,6 +316,7 @@
 <style scoped lang="less">
   .mui-content {
     background: #ffffff;
+    bottom: 1.333rem;
   }
   .topImg {
     margin-bottom: 0.32rem;
@@ -253,6 +324,12 @@
       width: 10rem;
       height: 5.333rem;
     }
+  }
+  .allDianPing {
+    padding: 0 0.426rem;
+    color: #444444;
+    font-size: 0.426rem;
+    line-height: 1.173rem;
   }
   .detailTitle {
     font-size: 0.506rem;
@@ -349,6 +426,70 @@
       }
     }
   }
+  .productList {
+    .comment-product {
+      padding: 0.346rem 0.4rem 0;
+      .product-info {
+        padding: 0 0 0.4rem;
+        background: none;
+      }
+      .productName {
+        width: 7.573rem;
+      }
+    }
+  }
+  .comment-product {
+    padding: 0.293rem 0.4rem 0.4rem;
+    .product-info {
+      overflow: hidden;
+      background: #F7F8FA;
+      padding: 0.266rem;
+      .product-img {
+        width: 1.173rem;
+        height: 1.173rem;
+        float: left;
+        img {
+          width: 100%;
+          height: 100%;
+          border-radius: 0.106rem;
+          object-fit: cover;
+        }
+      }
+      .product-detail {
+        float: left;
+        margin-left: 0.266rem;
+        .productName {
+          color: #444444;
+          font-size: 0.426rem;
+          line-height: 0.6rem;
+        }
+        .productMark {
+          .icon {
+            color: #FCC816;
+            font-size: 0.32rem;
+          }
+          span {
+            color: #B4B4B6;
+            font-size: 0.293rem;
+            line-height: 0.4rem;
+            &:nth-of-type(1) {
+              color: #FCC816;
+              margin-left: 0.08rem;
+            }
+          }
+          i {
+            width: 0.053rem;
+            height: 0.053rem;
+            margin-right: 0.133rem;
+            vertical-align: middle;
+            border-radius: 50%;
+            background: #B4B4B6;
+            display: inline-block;
+          }
+        }
+      }
+    }
+  }
 
   .mui-table-view-cell {
     padding-top: 0.133rem;
@@ -366,5 +507,95 @@
   .lineMargin {
     margin-top: -0.426rem;
     margin-bottom: 0.373rem;
+  }
+
+  .container-footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 1.333rem;
+    overflow: hidden;
+    /*padding: 0.36rem 0.426rem;*/
+    background: #FFFFFF;
+    &:before {
+      position: absolute;
+      top: 0;
+      width: 100%;
+      height: .02667rem;
+      content: '';
+      -webkit-transform: scaleY(.5);
+      transform: scaleY(.5);
+      background-color: #dcdcdc;
+    }
+    .footerLeft {
+      display: flex;
+      font-size: 0.373rem;
+      float: left;
+      padding: 0.186rem 0 0.186rem 0.426rem;
+      .footerMenuTwo {
+        display: flex;
+        .containerBtn {
+          display: flex;
+          width: 2.96rem;
+          height: 0.96rem;
+          color: #ffffff;
+          margin-right: 0.133rem;
+          line-height: 0.96rem;
+          text-align: center;
+          border-radius: 0.213rem;
+          justify-content: center;
+        }
+        .noBullish {
+          background: #FA4975;
+        }
+        .bullish {
+          background: #03AEF9;
+        }
+      }
+      .footerMenuOne {
+        width: 5.933rem;
+        height: 0.96rem;
+        display: flex;
+        color: #03AEF9;
+        line-height: 0.96rem;
+        border-radius: 0.213rem;
+        background: #F3F4F6;
+        text-align: center;
+        justify-content: center;
+        &.activeRed {
+          color: #FA4975;
+        }
+        &.activeBlue {
+          color: #03AEF9;
+        }
+      }
+    }
+    .footerRight {
+      color: #B4B4B6;
+      font-size: 0.266rem;
+      text-align: center;
+      display: flex;
+      float: right;
+      margin-top: 0.186rem;
+      margin-right: 0.426rem;
+      .collectionComment {
+        width: 1.333rem;
+        height: 1.306rem;
+        flex-grow:1;
+        color: #808080;
+        span {
+          color: #B4B4B6;
+          display: block;
+          margin-top: -0.106rem;
+          i {
+            font-style: normal;
+          }
+        }
+      }
+      .icon {
+        font-size: 0.586rem;
+        /*margin-top: -0.106rem;*/
+      }
+    }
   }
 </style>
