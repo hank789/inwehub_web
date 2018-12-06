fba967053287d11dc210e47160e5ec3add14180f
edwin <edwincoder@163.com>
2018-12-06 15:58:43 +0800

    首页banner 左右留3px间距

--- asset/views/Home.vue
+++ asset/views/Home.vue
@@ -32,15 +32,15 @@
           <div id="home_banner_slider" class="homeMuiSlider mui-slider" v-if="data.banners.length">
             <div class="mui-slider-group  mui-slider-loop">
               <div class="mui-slider-item mui-slider-item-duplicate" v-if="data.banners[data.banners.length-1]">
-                <a @tap.stop.prevent="goLink(data.banners[data.banners.length-1].url)"><ImageView :src="data.banners[data.banners.length-1].img_url" width="343" :isLazyload="false"></ImageView></a>
+                <div @tap.stop.prevent="goLink(data.banners[data.banners.length-1].url)"><ImageView :src="data.banners[data.banners.length-1].img_url" width="343" height="136" :isLazyload="false"></ImageView></div>
               </div>
               <div class="mui-slider-item" v-for="(notice, index) in data.banners">
-                <a  @tap.stop.prevent="goLink(notice.url)" target="_blank"><ImageView :src="notice.img_url" width="343" :isLazyload="false"></ImageView></a>
+                <div  @tap.stop.prevent="goLink(notice.url)" target="_blank"><ImageView :src="notice.img_url" width="343" height="136" :isLazyload="false"></ImageView></div>
               </div>
               <div class="mui-slider-item mui-slider-item-duplicate" v-if="data.banners[0]">
-                <a @tap.stop.prexvent="goLink(data.banners[0].url)">
-                  <ImageView :src="data.banners[0].img_url" width="343" :isLazyload="false"></ImageView>
-                </a>
+                <div @tap.stop.prexvent="goLink(data.banners[0].url)">
+                  <ImageView :src="data.banners[0].img_url" width="343" height="136" :isLazyload="false"></ImageView>
+                </div>
               </div>
             </div>
             <div class="home mui-slider-indicator">
@@ -386,7 +386,9 @@
   .mui-scrollbar-vertical{
     display: none !important;
   }
-
+  .homeMuiSlider.mui-slider .mui-slider-group .mui-slider-item{
+    margin:0 0.08rem;
+  }
   .homeMuiSlider.mui-slider .mui-slider-group .mui-slider-item img{
     height:3.626rem !important;
     object-fit: cover;
