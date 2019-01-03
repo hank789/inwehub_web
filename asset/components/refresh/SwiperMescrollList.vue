8155b0ffd64c9b473746c2691379ad3f5cda32b3
edwin <edwincoder@163.com>
2019-01-03 15:02:44 +0800

    swipermescrollList

--- asset/components/refresh/SwiperMescrollList.vue
+++ asset/components/refresh/SwiperMescrollList.vue
@@ -1,7 +1,6 @@
 <template>
-  <swiper ref="inTags" :options="swiperOption" class="mescrollList-swiper-container">
+  <swiper ref="mySwiper" :options="swiperOption" class="mescrollList-swiper-container">
     <swiper-slide v-for="(config, index) in listDataConfig" :key="index">
-
       <RefreshList
         :ref="'RefreshList_' + index"
         class="refreshListWrapper"
@@ -12,11 +11,11 @@
         :pageMode="true"
         :autoShowEmpty="false"
       >
-        <div v-for="(list, listIndex) in lists">
-          <div v-for="(item, itemIndex) in list">
+
+        <div v-for="(item, itemIndex) in lists[index]" :key="itemIndex">
             {{ item.id }}
-          </div>
         </div>
+
       </RefreshList>
     </swiper-slide>
   </swiper>
@@ -25,6 +24,7 @@
 <script>
   import RefreshList from './MescrollList.vue'
   import { swiper, swiperSlide } from 'vue-awesome-swiper'
+  import Vue from 'vue'
 
   const SwiperMescrollList = {
     data () {
@@ -35,9 +35,11 @@
           spaceBetween: 0,
           observer: true,
           freeMode: true,
-          onTransitionEnd: (swiper) => {
-            var i = swiper.activeIndex // 轮播切换完毕的事件
-            this.changePage(i)
+          on: {
+            transitionEnd: () => {
+              var i = this.swiper.activeIndex
+              this.changePage(i)
+            }
           }
         },
         lists: []
@@ -56,8 +58,14 @@
         }
       }
     },
+    computed: {
+      swiper () {
+        return this.$refs.mySwiper.swiper
+      }
+    },
     methods: {
       listChange (list, index) {
+        Vue.set(this.lists, index, list)
         this.lists[index] = list
         this.$emit('input', this.lists)
       },
@@ -68,11 +76,7 @@
         this.curNavIndex = i
 
         // 定位菜单
-        var eles = document.querySelectorAll('.mescroll-list-swiper-nav')
-        for (var j in eles) {
-          eles[j].classList.remove('active')
-        }
-        document.querySelector('.mescroll-list-swiper-nav-' + i).classList.add('active')
+        this.$emit('curNavIndexChange', this.curNavIndex)
       }
     }
   }
