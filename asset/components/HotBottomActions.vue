6744014a20789078541ea2e22209213107347084
陈灵灵 <798092775@qq.com>
2019-01-17 11:18:37 +0800

    app订阅

--- asset/components/HotBottomActions.vue
+++ asset/components/HotBottomActions.vue
@@ -12,7 +12,7 @@
         </div>
         <div class="right">
           <div class="oneLine"></div>
-          <div class="menu" @tap.stop.prevent="appPush">
+          <div class="menu" @tap.stop.prevent="subscribeApp">
                 <span class="iconCircular one" :class="isOpenAppPush === 1 ? 'grey':''">
                   <svg class="icon" aria-hidden="true">
                     <use xlink:href="#icon-xiazaiapp"></use>
@@ -71,6 +71,15 @@
     watch: {
     },
     methods: {
+      subscribeApp () {
+        if (!this.isOpenAppPush) {
+          setHotRecommendAppPushStatus(1, () => {
+            this.isOpenAppPush = 1
+            window.mui.toast('“APP订阅”成功')
+          }, () => {
+          })
+        }
+      },
       subscribeEmail () {
         if (!this.isOpenEmailPush) {
           if (!this.emailText) {
