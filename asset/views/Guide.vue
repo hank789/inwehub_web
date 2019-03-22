38db0f843dfad68671927178749d797cfda367dc
hank <hank.huiwang@gmail.com>
2019-03-22 18:19:37 +0800

    引导页后不显示ad页

--- asset/views/Guide.vue
+++ asset/views/Guide.vue
@@ -70,7 +70,7 @@
       document.getElementById('close').addEventListener('tap', function (event) {
         localEvent.setLocalItem('lauchFlag', {showGuide: true})
         closeFullscreen()
-        t.$router.replace('/ad')
+        t.$router.replace('/home')
       }, false)
 
       // 图片切换时，触发动画
